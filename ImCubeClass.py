fixes to some masking

ImCubeClass.py
@@ -126,7 +126,10 @@ class ImCube:
         return self
     
     def getMeanSpectra(self,mask = None):
-        return self._data[mask].mean(axis=0)
+        if mask is None:
+            return self._data.mean(axis=0).mean(axis=0)
+        else:
+            return self._data[mask].mean(axis = 0)
     
     def selectROI(self,typ = 'rect',xSlice = None,ySlice = None):
         mask = np.zeros((self._data.shape[0],self._data.shape[1]),dtype=np.bool)
@@ -144,6 +147,7 @@ class ImCube:
             except:
                 raise TypeError("A valid ROI type was not indicated. please use 'rect' or 'lasso'.")
             fig,ax = self.plotMean()
+            fig.suptitle("Close to accept ROI")
             x,y = np.meshgrid(np.arange(self._data.shape[0]),np.arange(self._data.shape[1]))
             coords = np.vstack((x.flatten(),y.flatten())).T
             mask = np.zeros((self._data.shape[0],self._data.shape[1]),dtype=np.bool)
@@ -152,18 +156,10 @@ class ImCube:
                 p = path.Path(verts)
                 ind = p.contains_points(coords,radius=0)
                 mask[coords[ind,1],coords[ind,0]] = True
-                plt.figure()
-                plt.imshow(mask)
-                plt.close(fig)
             def rectSelect(mins,maxes):
                 y = [int(mins.ydata),int(maxes.ydata)]
                 x = [int(mins.xdata),int(maxes.xdata)]
-                print(x)
-                print(y)
                 mask[min(y):max(y),min(x):max(x)] = True
-                plt.figure()
-                plt.imshow(mask)
-                plt.close(fig)
             if typ == 'lasso':
                 lasso = widgets.LassoSelector(ax,onSelect)
             else: