21107d86fa26ebdec26d55477db30412ba462b1d
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-09-14 16:48:11 -0500

    support timestamps from old binary files.

--- ImCubeClass.py
+++ ImCubeClass.py
@@ -32,8 +32,8 @@ class ImCube:
             info3 = list(loadmat(os.path.join(directory,'info3.mat'))['info3'].squeeze())
             wv = list(loadmat(os.path.join(directory,'wv.mat'))['WV'].squeeze())
             md = {'startWv':info2[0],'stepWv':info2[1],'stopWv':info2[2],
-                 'exposure':info2[3],'time':'Unkn','systemId':info3[0],
-                 'imgHeight':int(info3[2]),'imgWidth':int(info3[3]),'waveLengths':wv}
+                 'exposure':info2[3],'time':'{:d}-{:d}-{:d} {:d}:{:d}:{:d}'.format(*[int(i) for i in [info3[8],info3[7],info3[6],info3[9],info3[10],info3[11]]]),'systemId':info3[0],
+                 'imgHeight':int(info3[2]),'imgWidth':int(info3[3]),'wavelengths':wv}
         with open(os.path.join(directory,'image_cube'),'rb') as f:
             data = np.frombuffer(f.read(),dtype=np.uint16)
         data = data.reshape((md['imgHeight'],md['imgWidth'],len(md['wavelengths'])),order='F')
