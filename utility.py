b023e7b6c5f695c086487bcdb4d7b8efc03c6ab8
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-10-11 12:56:49 -0500

    add a non parallel file loader/processor

--- utility.py
+++ utility.py
@@ -8,6 +8,7 @@ from pwspython import ImCube
 import psutil
 import multiprocessing as mp
 import threading as th
+import queue
 
 def loadAndProcessParallel(fileDict:dict, processorFunc = None):
     def loadIms(fileDict):
@@ -40,6 +41,38 @@ def loadAndProcessParallel(fileDict:dict, processorFunc = None):
         po = mp.Pool(processes = psutil.cpu_count(logical=False)-1)
         cubes = po.map(processorFunc, [q]*numIms)
         thread.join()
+    else:
+        cubes = [q.get() for i in range(numIms)]
+    return cubes
+
+def loadAndProcess(fileDict:dict, processorFunc = None):
+    def loadIms(fileDict):
+        for t, v in fileDict.items():
+            for s, files in v.items():    
+                for file in files:
+                    _ =ImCube.loadAny(file)
+                    _.type = t
+                    _.setting = s
+                    _.exposure = _.metadata['exposure']
+                    q.put(_)
+                    perc = psutil.virtual_memory().percent
+                    print(file)
+                    print("Memory Usage: ", perc,'%')
+                    if perc >= 95:
+                        del cubes
+                        print('quitting')
+                        quit()  
+    numIms = 0
+    for i, v in fileDict.items():
+        for j, lis in v.items():
+            numIms += len(lis)
+    q = queue.Queue()
+    thread = th.Thread(target = loadIms, args=[fileDict])
+    thread.start()
+
+    if processorFunc is not None:
+        #%% Start processing in parallel
+        cubes = [processorFunc(q) for i in range(numIms)]
     else:
         cubes = [q.get() for i in range(numIms)]
     return cubes
