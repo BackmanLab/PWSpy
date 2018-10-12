fbb0ee08aacb1c9fa32f704a9b28ae686fd35b14
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-10-12 16:45:24 -0500

    Made parallel loader much more flexible using recursive function and a argument to specify the specifier names.

--- utility.py
+++ utility.py
@@ -9,15 +9,22 @@ import psutil
 import multiprocessing as mp
 import threading as th
 import queue
+import typing
 
-def loadAndProcessParallel(fileDict:dict, processorFunc = None):
-    def loadIms(fileDict):
-        for t, v in fileDict.items():
-            for s, files in v.items():    
-                for file in files:
+def loadAndProcessParallel(fileDict:dict, processorFunc = None, specifierNames:list = None):
+    def loadIms(fileDict, specifierNames):
+        def a(arg, specifiers:typing.List[str] = []):
+            if isinstance(arg,dict):
+                for k,v in arg.items():
+                    a(v,specifiers + [k])
+            elif isinstance(arg,list):
+                for file in arg:
                     _ =ImCube.loadAny(file)
-                    _.type = t
-                    _.setting = s
+                    if specifierNames is None:
+                        _.specifiers = specifiers
+                    else:
+                        for i,name in enumerate(specifierNames):
+                            setattr(_,name,specifiers[i])
                     _.exposure = _.metadata['exposure']
                     q.put(_)
                     perc = psutil.virtual_memory().percent
@@ -27,13 +34,17 @@ def loadAndProcessParallel(fileDict:dict, processorFunc = None):
                         del cubes
                         print('quitting')
                         quit()  
+            else:
+                raise TypeError(f'Filedict must only contain Dict and List, not an item of type: {type(arg)}')
+        a(fileDict)
+
     numIms = 0
     for i, v in fileDict.items():
         for j, lis in v.items():
             numIms += len(lis)
     m = mp.Manager()
     q = m.Queue()
-    thread = th.Thread(target = loadIms, args=[fileDict])
+    thread = th.Thread(target = loadIms, args=[fileDict, specifierNames])
     thread.start()
 
     if processorFunc is not None:
