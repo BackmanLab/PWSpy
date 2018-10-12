7911ef586b74f98f08d9b26b4b3261553e72e375
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-10-12 16:52:53 -0500

    Combined the two loader functions into one with a parallel option

--- utility.py
+++ utility.py
@@ -8,11 +8,10 @@ from pwspython import ImCube
 import psutil
 import multiprocessing as mp
 import threading as th
-import queue
 import typing
 
-def loadAndProcessParallel(fileDict:dict, processorFunc = None, specifierNames:list = None):
-    def loadIms(fileDict, specifierNames):
+
+def _loadIms(q, fileDict, specifierNames):
         def a(arg, specifiers:typing.List[str] = []):
             if isinstance(arg,dict):
                 for k,v in arg.items():
@@ -38,52 +37,24 @@ def loadAndProcessParallel(fileDict:dict, processorFunc = None, specifierNames:l
                 raise TypeError(f'Filedict must only contain Dict and List, not an item of type: {type(arg)}')
         a(fileDict)
 
+def loadAndProcess(fileDict:dict, processorFunc = None, specifierNames:list = None, parallel = False):
     numIms = 0
     for i, v in fileDict.items():
         for j, lis in v.items():
             numIms += len(lis)
     m = mp.Manager()
     q = m.Queue()
-    thread = th.Thread(target = loadIms, args=[fileDict, specifierNames])
+    thread = th.Thread(target = _loadIms, args=[q, fileDict, specifierNames])
     thread.start()
 
     if processorFunc is not None:
-        #%% Start processing in parallel
-        po = mp.Pool(processes = psutil.cpu_count(logical=False)-1)
-        cubes = po.map(processorFunc, [q]*numIms)
+        #%% Start processing
+        if parallel:
+            po = mp.Pool(processes = psutil.cpu_count(logical=False)-1)
+            cubes = po.map(processorFunc, [q]*numIms)
+        else:
+            cubes = [processorFunc(q) for i in range(numIms)]
         thread.join()
-    else:
-        cubes = [q.get() for i in range(numIms)]
-    return cubes
-
-def loadAndProcess(fileDict:dict, processorFunc = None):
-    def loadIms(fileDict):
-        for t, v in fileDict.items():
-            for s, files in v.items():    
-                for file in files:
-                    _ =ImCube.loadAny(file)
-                    _.type = t
-                    _.setting = s
-                    _.exposure = _.metadata['exposure']
-                    q.put(_)
-                    perc = psutil.virtual_memory().percent
-                    print(file)
-                    print("Memory Usage: ", perc,'%')
-                    if perc >= 95:
-                        del cubes
-                        print('quitting')
-                        quit()  
-    numIms = 0
-    for i, v in fileDict.items():
-        for j, lis in v.items():
-            numIms += len(lis)
-    q = queue.Queue()
-    thread = th.Thread(target = loadIms, args=[fileDict])
-    thread.start()
-
-    if processorFunc is not None:
-        #%% Start processing in parallel
-        cubes = [processorFunc(q) for i in range(numIms)]
     else:
         cubes = [q.get() for i in range(numIms)]
     return cubes
