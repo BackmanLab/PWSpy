4caf91e0b7e5e4634c01b57d61850d2e6911dbc6
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-10-30 15:13:58 -0500

    Added an extra reflection plotting routine.

--- utility.py
+++ utility.py
@@ -4,13 +4,17 @@ Created on Thu Oct 11 11:31:48 2018
 
 @author: backman05
 """
-from pwspython import ImCube
+from pwspython import ImCube, reflectanceHelper
+from glob import glob
+import matplotlib.pyplot as plt
+import numpy as np
 import psutil
 import multiprocessing as mp
 import threading as th
 import typing
 import os
 from time import time
+import pandas as pd
 
 
 def _loadIms(q, fileDict, specifierNames):
@@ -75,3 +79,65 @@ def loadAndProcess(fileDict:dict, processorFunc = None, specifierNames:list = No
     thread.join()
     print(f"Loading took {time()-sTime} seconds")
     return cubes
+
+def plotExtraReflection(types:list, settings:list, rootDir:str, processorFunc:callable, save = False):
+    fileDict = {t:{s:glob(os.path.join(rootDir,t,s,'Cell*')) for s in settings} for t in types}
+    cubes = loadAndProcess(fileDict, processorFunc, specifierNames = ['type', 'setting'], parallel = True)
+
+
+    mask = [i for i in cubes if i.setting == settings[-1]][0].selectROI()
+    
+    #%% load theory reflectance
+    AirR = reflectanceHelper.getReflectance('air','glass', index = cubes[0].wavelengths)
+    WaterR = reflectanceHelper.getReflectance('water','glass',index = cubes[0].wavelengths)
+    #%% Select cubes
+    subs = [i for i in cubes if i.type == types[-1]]
+    cubes = [i for i in cubes if i.type != types[-1]]
+    
+    
+    #%% plot
+    factors = {}
+    reflections = {}
+    fig, ax = plt.subplots() #For extra reflections
+    plt.title("Extra Reflection")
+    ax.set_ylabel("%")
+    ax.set_xlabel("nm")
+    fig2, ax2 = plt.subplots() # for correction factor
+    plt.title("Uncorrected A/W reflection ratio")
+
+    for sett in settings:
+        scubes = [i for i in cubes if (i.setting == sett)] #select out some images
+        airCubes = [i for i in scubes if i.type == types[0]]
+        waterCubes = [i for i in scubes if i.type == types[1]]
+    
+        allCombos = []
+        for air in airCubes:
+            for wat in waterCubes:
+                    allCombos.append((air,wat))
+        
+        Rextras = []
+        for air,water in allCombos:
+            Rextras.append(((AirR * water.getMeanSpectra(mask)[0]) - (WaterR * air.getMeanSpectra(mask)[0])) / (air.getMeanSpectra(mask)[0] - water.getMeanSpectra(mask)[0]))
+            ax.plot(air.wavelengths, Rextras[-1], label = '{} Air:{}ms Water:{}ms'.format(sett, int(air.exposure), int(water.exposure)))
+
+        for air,water in allCombos:
+            plt.figure()
+            plt.title("{}, Air:{}ms, Water:{}ms".format(sett, air.exposure, water.exposure))
+            refIm = (((AirR[np.newaxis,np.newaxis,:] * water.data) - (WaterR[np.newaxis,np.newaxis,:] * air.data)) / (air.data - water.data)).mean(axis=2)
+            plt.imshow(refIm,vmin=np.percentile(refIm,.5),vmax=np.percentile(refIm,99.5))
+            plt.colorbar()
+            ax2.plot(air.getMeanSpectra(mask)[0]/water.getMeanSpectra(mask)[0], label=sett)
+        
+        print("{} correction factor".format(sett))
+        Rextra = np.array(Rextras)
+        factors[sett] = (Rextra.mean() + WaterR.mean()) / WaterR.mean()
+        reflections[sett] = Rextra.mean(axis = 0)
+        print(factors[sett])
+    ax.legend()
+    ax2.legend()
+    
+    df = pd.DataFrame(factors, index = [0])
+    df2 = pd.DataFrame(reflections,index = cubes[0].wavelengths)
+    if save:
+        df.to_csv(os.path.join(rootDir, 'calcReflections','factors.csv'))
+        df2.to_csv(os.path.join(rootDir, 'calcReflections','reflections.csv'))
