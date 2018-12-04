cd8607d8b8b25f9fa40c90fcda293a8ee4fc10bc
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-12-04 13:02:22 -0600

    got the mirror and offset functions working

--- micromanagerPositions.py
+++ micromanagerPositions.py
@@ -7,6 +7,7 @@ Created on Mon Dec  3 17:53:24 2018
 
 import json
 import typing
+import numpy as np
 
 class Property:
     def __init__(self, name:str, pType:str, value):
@@ -35,51 +36,65 @@ class Position2d:
     def __init__(self, x:float, y:float, xyStage:str, label:str):
         self.x = x
         self.y = y
+        self.xyStage = xyStage
+        self.label = label
+        self._regen()
+
+    def _regen(self):
         contents = [
-                Property("DefaultXYStage", "STRING", xyStage),
-                Property("DefaultZStage", "STRING", ""),
-                PropertyMap("DevicePositions",
-                            [Property("Device", "STRING", xyStage),
-                            Property("Position_um", "DOUBLE", [self.x, self.y])]),
-                Property("GridCol", "INTEGER", 0),
-                Property("GridRow", "INTEGER", 0),
-                Property("Label", "STRING",label)]
+            Property("DefaultXYStage", "STRING", self.xyStage),
+            Property("DefaultZStage", "STRING", ""),
+            PropertyMap("DevicePositions",
+                        [Property("Device", "STRING", self.xyStage),
+                        Property("Position_um", "DOUBLE", [self.x, self.y])]),
+            Property("GridCol", "INTEGER", 0),
+            Property("GridRow", "INTEGER", 0),
+            Property("Label", "STRING",self.label)]
         self._d = {i.name:i for i in contents}
         
     def mirrorX(self):
         self.x *= -1
+        self._regen()
+
     def mirrorY(self):
         self.y *= -1
+        self._regen()
+
     def addOffset(self, dx, dy):
         self.x += dx
         self.y += dy
-    
+        self._regen()
+
 class PositionList:
     def __init__(self, positions: typing.List[Position2d]):
         self.positions = positions
+        self._regen()
+    def _regen(self):
         self._d = {"encoding": "UTF-8",
-                   'format': 'Micro-Manager Property Map',
-                   'major_version': 2,
-                   'minor_version': 0,
-                   "map": {"StagePositions": PropertyMap("StagePositions", self.positions)}}
+           'format': 'Micro-Manager Property Map',
+           'major_version': 2,
+           'minor_version': 0,
+           "map": {"StagePositions": PropertyMap("StagePositions", self.positions)}}
     def mirrorX(self):
         for i in self.positions:
             i.mirrorX()
+        self._regen()
     def mirrorY(self):
         for i in self.positions:
             i.mirrorY()
+        self._regen()
     def addOffset(self, dx, dy):
         for i in self.positions:
             i.addOffset(dx, dy)
+        self._regen()
+    def save(self, savePath):
+        #    a=json.dumps(plist,cls=Encoder, ensure_ascii=False)
+        #    a = a.replace('{','{\n').replace('[','[\n').replace('}','\n}').replace(',',',\n').replace(']','\n]')
+        with open(savePath+'.pos','w') as f:
+            json.dump(self,f,cls=Encoder)
             
 class Encoder(json.JSONEncoder):
     def default(self,obj):
-#        if isinstance(obj, set):
-#            st = '{'
-#            for i in obj:
-#                st += json.dumps(i, cls=Encoder, ensure_ascii=False) + ','
-#            st += '}'
-#            return st
         if isinstance(obj, PositionList):
             return obj._d
         elif isinstance(obj, Position2d):
@@ -87,22 +102,30 @@ class Encoder(json.JSONEncoder):
         elif isinstance(obj, PropertyMap):
             return obj._d
         elif isinstance(obj, Property):
-            return obj._d#obj.name+':'+json.dumps(obj._d, cls=Encoder, ensure_ascii=False)
+            return obj._d
         else:
             return json.JSONEncoder(ensure_ascii=False).default(self, obj)
+        
+def generateList(data):
+    assert isinstance(data,np.ndarray)
+    assert len(data.shape)==2
+    assert data.shape[1]==2
+    positions=[]
+    for n,i in enumerate(data):
+        positions.append(Position2d(*i,'TIXYDrive',f'Cell{n+1}'))
+    plist = PositionList(positions)
+    return plist
+    
     
 if __name__ == '__main__':
-#    p=Property('hey','STRING','323')
-#    p2 = PropertyMap('hah',[p])
-#    pos = Position2d(1,2,'re','rara')
-#    a = json.dumps(pos,cls=Encoder)
-    pos=[[0,0],
-     [1,1],
-     [2,3]]
+
+#    pos=[[0,0],
+#     [1,1],
+#     [2,3]]
     
     positions=[]
     for n,i in enumerate(pos):
-        positions.append(Position2d(*i,'XY',str(n)))
+        positions.append(Position2d(*i,'XY',f'Cell{n+1}'))
     plist = PositionList(positions)
     a=json.dumps(plist,cls=Encoder, ensure_ascii=False)
     a = a.replace('{','{\n').replace('[','[\n').replace('}','\n}').replace(',',',\n').replace(']','\n]')
