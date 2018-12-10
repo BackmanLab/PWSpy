e82d2b7898e5e84a07f0a861eac9d98d81387e6f
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-12-10 10:24:26 -0600

    change syntheticMirror to a different approach that works much better.

--- examples/syntheticMirror.py
+++ examples/syntheticMirror.py
@@ -9,10 +9,14 @@ from pwspython import ImCube
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal as sps
+import time
+import copy
 
 def gaussKernel(radius:int):
+    #A kernel that goes to 1 std. It would be better to go out to 2 or 3 std but then you need a larger kernel which greatly increases convolution time.
     lenSide = 1+2*radius
-    X,Y = np.meshgrid(np.linspace(-3,3,num=lenSide), np.linspace(-3,3,num=lenSide))
+    side = np.linspace(-1,1,num=lenSide)
+    X,Y = np.meshgrid(side, side)
     R = np.sqrt(X**2 + Y**2)
     k = np.exp(-(R**2)/2)
     k = k/k.sum() #normalize so the total is 1.
@@ -22,21 +26,32 @@ a = ImCube.fromTiff(r'G:\Vasundhara_MSCs_data\Cell1170')
 m = ImCube.fromTiff(r'G:\Vasundhara_MSCs_data\Cell9999')
 #(a/m).plotMean() #The mirror is bad so we need to make our own
 
-print("Select an ROI near the center that doesnt contain cells to get the mirror spectrum")
-mask = (a/m).selectROI()
-spec = a.getMeanSpectra(mask)[0]
+#print("Select an ROI near the center that doesnt contain cells to get the mirror spectrum")
+#mask = (a/m).selectROI()
+#spec = a.getMeanSpectra(mask)[0]
+#B,A = sps.butter(2, 0.1)
+#arr = sps.filtfilt(B,A,a.data, axis=2)#filter along lambda to get rid of sigma
+#amp = arr.max(axis=2) #Get the maximum intensity across the field of view.
+#kernel = gaussKernel(90)
+#print("Start Convolving")
+#time.sleep(0.1)
+#amp = sps.convolve2d(amp, kernel, mode='same', fillvalue = amp.mean())    #Filter along the field of view to remove the cells.
+#print("Done")
+#time.sleep(0.1)
+#amp = amp / amp.max()   #normalize to one
+#mirror = ImCube(spec[np.newaxis,np.newaxis,:] * amp[:,:,np.newaxis], a.metadata)
 
-B,A = sps.butter(2, 0.1)
-arr = sps.filtfilt(B,A,a.data, axis=2)#filter along lambda to get rid of sigma
-amp = arr.max(axis=2) #Get the maximum intensity across the field of view.
-kernel = gaussKernel(200)
-print("Start Convolving")
-amp = sps.convolve2d(amp, kernel, mode='same', fillvalue = amp.mean())    #Filter along the field of view to remove the cells.
-print("Done")
-amp = amp / amp.max()   #normalize to one
-
-
-mirror = ImCube(spec[np.newaxis,np.newaxis,:] * amp[:,:,np.newaxis], a.metadata)
+kernel = gaussKernel(10)
+mirror = copy.deepcopy(a) #This doesn't work right. maybe becuaes of the wi
+for i in range(a.data.shape[2]):
+    print(i)
+    time.sleep(0.1)
+    mirror.data[:,:,i] = sps.convolve2d(mirror.data[:,:,i],kernel,mode='same',fillvalue=mirror.data[:,:,i].mean())
+    
 a.plotMean()
 mirror.plotMean()
-(a/mirror).plotMean()
+norm = (a/mirror)
+norm.plotMean()
+plt.figure()
+plt.imshow(norm.data.std(axis=2))
+
