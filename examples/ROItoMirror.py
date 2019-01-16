5700a0794a9063339dbf89cf0c6d4b985baf7bfb
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-01-16 17:15:53 -0600

    Add example of making a mirror from the average spectra in an ROI

---
+++ examples/ROItoMirror.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  9 15:00:39 2019
+
+@author: backman05
+"""
+
+
+from pwspython import ImCube
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+'''
+This script Allows the user to select a region of an ImCube. the spectra of this
+region is then averaged over the X and Y dimensions. This spectra is then saved
+as a reference imCube with the same initial dimensions.
+'''
+
+
+a = ImCube.loadAny(r'G:\Calibrations\CellPhantom\lcpws1\5th\Cell2')
+
+mask = a.selectLassoROI()
+spec, std = a.getMeanSpectra(mask)
+newData = np.zeros(a.data.shape)
+newData[:,:,:] = spec[np.newaxis, np.newaxis, :]
+ref = ImCube(newData, a.metadata)
+
+plt.plot(a.wavelengths, spec)
