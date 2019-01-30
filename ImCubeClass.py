d301513339958c41cc672dd0b4f319a26058a4ed
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-01-30 14:30:29 -0600

    add metadata only class

--- ImCubeClass.py
+++ ImCubeClass.py
@@ -428,4 +428,60 @@ class KCube(ImCube):
         raise NotImplementedError
     def _wavelengthsMatch(self, other:KCube) -> bool:
         return self.wavenumbers == other.wavenumbers
+    
+class ICMetaData(dict):
+    def __init__(self, metadata):
+        assert isinstance(metadata,dict)
+        super().__init__(metadata)
+    @classmethod
+    def loadAny(cls, directory):
+        try:
+            return ICMetaData.fromTiff(directory)
+        except Exception as e:
+            try:
+                return ICMetaData.fromOldPWS(directory)
+            except:
+                raise Exception(f"Could not find a valid PWS image cube file at {directory}.")
+    @classmethod
+    def fromOldPWS(cls,directory):
+        try:
+            md = json.load(open(os.path.join(directory,'pwsmetadata.txt')))
+        except: #have to use the old metadata
+            print("Json metadata not found")
+            info2 = list(loadmat(os.path.join(directory,'info2.mat'))['info2'].squeeze())
+            info3 = list(loadmat(os.path.join(directory,'info3.mat'))['info3'].squeeze())
+            wv = list(loadmat(os.path.join(directory,'wv.mat'))['WV'].squeeze())
+            wv = [int(i) for i in wv] #We will have issues saving later if these are numpy int types.
+            md = {'startWv':info2[0],'stepWv':info2[1],'stopWv':info2[2],
+                 'exposure':info2[3],'time':'{:d}-{:d}-{:d} {:d}:{:d}:{:d}'.format(*[int(i) for i in [info3[8],info3[7],info3[6],info3[9],info3[10],info3[11]]]),'systemId':info3[0],
+                 'imgHeight':int(info3[2]),'imgWidth':int(info3[3]),'wavelengths':wv}      
+        cls._checkMetadata(md)
+        return cls(md)
 
+    @classmethod
+    def fromTiff(cls,directory):
+        if os.path.exists(os.path.join(directory,'MMStack.ome.tif')):
+            path = os.path.join(directory,'MMStack.ome.tif')
+        elif os.path.exists(os.path.join(directory,'pws.tif')):
+            path = os.path.join(directory,'pws.tif')
+        else:
+            raise OSError("No Tiff file was found at:", directory)    
+        if os.path.exists(os.path.join(directory,'pwsmetadata.json')):
+            metadata = json.load(open(os.path.join(directory,'pwsmetadata.json'),'r'))
+        else:
+            with tf.TiffFile(path) as tif:
+                try:
+                    metadata = json.loads(tif.pages[0].description)
+                except:
+                    metadata = json.loads(tif.imagej_metadata['Info']) #The micromanager saves metadata as the info property of the imagej imageplus object.
+        metadata['time'] = tif.pages[0].tags['DateTime'].value
+        if 'waveLengths' in metadata:
+            metadata['wavelengths'] = metadata['waveLengths']
+            del metadata['waveLengths']
+        cls._checkMetadata(metadata)
+        return cls(metadata)
+    def _checkMetadata(metadata):
+        required = ['time', 'exposure', 'wavelengths']
+        for i in required:
+            if i not in metadata:
+                raise ValueError(f"Metadata does not have a '{i}' field.")
