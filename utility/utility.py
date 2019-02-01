d98c1172701a91deed0271b20fee68768a30caef
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-02-01 12:05:45 -0600

    improvements to 3dplotter

--- utility/utility.py
+++ utility/utility.py
@@ -125,23 +125,26 @@ def plot3d(X):
     class IndexTracker(object):
         def __init__(self, ax, X):
             self.ax = ax
-            ax[0].set_title('use scroll wheel to navigate images')
+#            ax[0].set_title('use scroll wheel to navigate images')
             self.X = X
-            rows, cols, self.slices = X.shape
-            self.coords = (100,100, self.slices//2)
-            self.vline = ax[0].plot([100,100],[0,X.shape[0]],'r')[0]
-            self.hline = ax[0].plot([0,X.shape[1]],[100,100],'r')[0]
-            self.line2 = ax[1].plot([0,X.shape[1]],[100,100],'r')[0]
-            self.line3 = ax[2].plot([100,100],[0,X.shape[0]],'r')[0]
-            self.line4 = ax[3].plot([0,X.shape[2]],[100,100],'r')[0]
             self.max = np.percentile(self.X,99.9)
             self.min = np.percentile(self.X,0.1)
+            rows, cols, self.slices = X.shape
+            self.coords = (100,100, self.slices//2)
+            self.ax[0].get_xaxis().set_visible(False)
+            self.ax[0].get_yaxis().set_visible(False)
+            lw=0.5
+            self.vline = ax[0].plot([100,100],[0,X.shape[0]],'r',linewidth = lw)[0]
+            self.hline = ax[0].plot([0,X.shape[1]],[100,100],'r',linewidth = lw)[0]
+            self.line2 = ax[1].plot([0,X.shape[1]],[100,100],'r',linewidth = lw)[0]
+            self.line3 = ax[2].plot([100,100],[0,X.shape[0]],'r',linewidth = lw)[0]
+            self.line4 = ax[3].plot([self.min,self.max],[100,100],'r',linewidth = lw)[0]
             self.yplot = ax[1].plot(self.X[:,self.coords[1],self.coords[2]], np.arange(self.X.shape[0]))[0]
             self.xplot = ax[2].plot(np.arange(self.X.shape[1]),self.X[self.coords[0],:,self.coords[2]])[0]
             self.zplot = ax[3].plot(self.X[self.coords[0],self.coords[1],:], np.arange(self.X.shape[2]))[0]
             self.im = ax[0].imshow(self.X[:, :, self.coords[2]])
             self.im.set_clim(self.min,self.max)
-            self.cbar = plt.colorbar(self.im, ax=ax[0])
+#            self.cbar = plt.colorbar(self.im, ax=ax[0])
             self.auto=perpetualTimer(0.2,self)
             self.update()  
         def onscroll(self, event):
@@ -156,7 +159,11 @@ def plot3d(X):
             if event.inaxes==self.ax[0]:
                 self.coords = (int(event.xdata), int(event.ydata), self.coords[2])
             elif event.inaxes==self.ax[1]:
+                self.coords = (self.coords[0], int(event.ydata), self.coords[2])
+            elif event.inaxes==self.ax[2]:
                 self.coords = (int(event.xdata), self.coords[1], self.coords[2])
+            elif event.inaxes==self.ax[3]:
+                self.coords = (self.coords[0], self.coords[1], int(event.ydata))
             
             self.update()
         def increment(self):
@@ -169,23 +176,31 @@ def plot3d(X):
             self.ax[1].set_xlim(min(self.yplot.get_data()[0]), max(self.yplot.get_data()[0]))
             self.xplot.set_data(np.arange(self.X.shape[1]),self.X[self.coords[0],:,self.coords[2]])
             self.ax[2].set_ylim(min(self.xplot.get_data()[1]), max(self.xplot.get_data()[1]))
-            self.zplot.set_data(self.X[self.coords[0],self.coords[1],:],np.arange(self.X.shape[2]))
-            self.ax[3].set_xlim(min(self.zplot.get_data()[0]),max(self.zplot.get_data()[0]))
+            self.zplot.set_data(self.X[self.coords[1],self.coords[0],:],np.arange(self.X.shape[2]))
+            self.ax[3].set_xlim(self.min,self.max)
             self.hline.set_data(self.hline.get_data()[0],[self.coords[1],self.coords[1]] )
             self.vline.set_data([self.coords[0],self.coords[0]],  self.vline.get_data()[1])
-            self.line3.set_data([self.coords[0],self.coords[0]], self.line3.get_data()[1])
-            self.line2.set_data( self.line2.get_data()[0],[self.coords[1],self.coords[1]])
+            self.line3.set_data([self.coords[0],self.coords[0]], self.ax[2].get_ylim())
+            self.line2.set_data( self.ax[1].get_xlim(),[self.coords[1],self.coords[1]])
             self.line4.set_data(self.line4.get_data()[0], [self.coords[2], self.coords[2]])
-            ax[0].set_ylabel('slice %s' % self.coords[2])
             self.im.axes.figure.canvas.draw()
             
     fig = plt.figure() 
     h,w,_ =X.shape 
-    gs = gridspec.GridSpec(2, 3,width_ratios=[w*.2,w,w*.2], height_ratios=[h,h*.2])
-    ax = [plt.subplot(gs[1]),]
-    ax.append(plt.subplot(gs[2], sharey=ax[0]))
-    ax.append(plt.subplot(gs[4], sharex=ax[0]))
-    ax.append(plt.subplot(gs[0]))
+    gs = gridspec.GridSpec(2, 3,hspace=0,width_ratios=[w*.2,w,w*.2], height_ratios=[h,h*.2])
+    ax = [plt.subplot(gs[0,1]),]
+    ax.append(plt.subplot(gs[0,2], sharey=ax[0]))
+    ax.append(plt.subplot(gs[1,1], sharex=ax[0]))
+    ax.append(plt.subplot(gs[0,0]))
+    ax[1].yaxis.set_ticks_position('right')
+
+    gs.update(wspace=0)
+
+#    gs.tight_layout(fig, h_pad=0, w_pad=0)
+#    fig.subplots_adjust(wspace = 0)
+#    fig.subplots_adjust(hspace = 0)
+
+
 #bounds = [x.min(),x.max(),y.min(),y.max()]
 #ax[0].imshow(data, cmap='gray', extent = bounds, origin='lower')
 #ax[1].plot(data[:,w/2],Y[:,w/2],'.',data[:,w/2],Y[:,w/2])
