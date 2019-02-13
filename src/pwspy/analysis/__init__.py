e7eeafaba28f27d8b0291ad3227918ab45d0100f
nanthony21 <nickmanthony@hotmail.com>
2019-02-12 22:28:35 -0600

    start an analysis module

---
+++ src/pwspy/analysis/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 21:44:31 2019
+
+@author: Nick
+"""
+
+from .analysisSettingsClass import AnalysisSettings
---
+++ src/pwspy/analysis/analysisSettingsClass.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 21:33:29 2019
+
+@author: Nick
+"""
+from typing import NamedTuple
+import json
+
+class AnalysisSettings(NamedTuple):
+    filterOrder:int
+    filterCutoff:float
+    polynomialOrder:int
+    rInternalSubtractionPath:str
+    referenceMaterial:str
+        
+    @classmethod
+    def fromJson(cls, filePath):
+        with open(filePath,'r') as f:
+            return cls(**json.load(f))
+        
+    def toJson(self,filePath):
+        with open(filePath,'w') as f:
+            json.dump(dict(self),f)
+                    
