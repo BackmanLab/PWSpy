ff202ac0628811ba6f9c1dfa0cc20450c705ff0a
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-02-16 17:44:55 -0600

    Big changes to the parallel loading stuff.made reading much faster when not in parallel. use two threads so the harddrive is always engaged. This is almost always better than parallel unless you have really heavy processing to do.

--- src/pwspy/utility/utility.py
+++ src/pwspy/utility/utility.py
@@ -12,64 +12,58 @@ import threading as th
 import typing
 import os
 from time import time
-from threading import Timer
-import matplotlib.gridspec as gridspec
-
+import sys
+import queue
 
 '''Local Functions'''
-def _loadIms(q, fileDict, specifierNames):
-        def a(arg, specifiers:typing.List[str] = []):
-            if isinstance(arg,dict):
-                for k,v in arg.items():
-                    a(v,specifiers + [k])
-            elif isinstance(arg,list):
-                for file in arg:
-                    fileSpecifiers = specifiers
-                    _ =ImCube.loadAny(file)
-                    if specifierNames is None:
-                        _.specifiers = fileSpecifiers
-                    else:
-                        for i,name in enumerate(specifierNames):
-                            setattr(_,name,fileSpecifiers[i])
-                    _.filename = os.path.split(file)[1]
-                    _.exposure = _.metadata['exposure']
-                    q.put(_)
-                    perc = psutil.virtual_memory().percent
-                    print(file)
-                    print("Memory Usage: ", perc,'%')
-                    if perc >= 95:
-                        del cubes
-                        print('quitting')
-                        quit()  
+def _recursiveSearch(fileDict, specifierNames, specifiers:typing.List[str]=[]):
+    results = []
+    if isinstance(fileDict,dict):
+        for k,v in fileDict.items():
+            results.extend(_recursiveSearch(v,specifiers + [k]))
+    elif isinstance(fileDict,list):
+        for file in fileDict:
+            fileSpecifiers = specifiers
+            if specifierNames is None:
+                results.append(({'specifiers':fileSpecifiers}, file))
             else:
-                raise TypeError(f'Filedict must only contain Dict and List, not an item of type: {type(arg)}')
-        a(fileDict)
+                results.append((dict(zip(specifierNames, fileSpecifiers)), file))
+    else:
+        raise TypeError(f'Filedict must only contain Dict and List[str], not an item of type: {type(arg)}')
+    return results
 
-def _countIms(fileDict):
-    def a(arg, numIms):
-        if isinstance(arg,dict):
-            for k,v in arg.items():
-                numIms = a(v,numIms)
-        elif isinstance(arg,list):
-            numIms += len(arg)
-            
-        else:
-            raise TypeError(f'Filedict must only contain Dict and List, not an item of type: {type(arg)}')
-        return numIms
-    return a(fileDict, 0)
-
-def _interpolateNans(arr):
-    def interp1(arr1):
-        nans = np.isnan(arr1)
-        f = lambda z: z.nonzero()[0]
-        arr1[nans] = np.interp(f(nans), f(~nans), arr1[~nans])
-        return arr1
-    arr = np.apply_along_axis(interp1, 2, arr)
-    return arr
+def _loadIms(qout, qin, lock):
+    while not qin.empty():
+        specs, file = qin.get()
+        print('starting',file)
+        lock.acquire()
+        im = ImCube.loadAny(file)
+        lock.release()
+        for k,v in specs.items():
+            setattr(im, k, v)
+        qout.put(im)
+        perc = psutil.virtual_memory().percent
+        print(file)
+        print("Memory Usage: ", perc,'%')
+        if perc >= 95:
+            print('quitting')
+            return
+        
+def loadThenProcess(procFunc,procFuncArgs, lock, fileAndSpecifiers):
+    specs, file = fileAndSpecifiers
+    lock.acquire()
+    im = ImCube.loadAny(file)
+    lock.release()
+    for k,v in specs.items():
+        setattr(im, k, v)
+    print("Run", file)
+    return procFunc(im, *procFuncArgs)
 
 '''User Functions'''
 def loadAndProcess(fileDict:dict, processorFunc = None, specifierNames:list = None, parallel = False, procArgs = []) -> typing.List[ImCube]:
     #Error checking
+    numThreads = 2 #even this might be unnecesary. don't bother going higher.
+    print("Starting loading")
     if not specifierNames is None:
         recursionDepth = 0
         fileStructure = fileDict
@@ -79,22 +73,35 @@ def loadAndProcess(fileDict:dict, processorFunc = None, specifierNames:list = No
         if recursionDepth != len(specifierNames):
             raise ValueError("The length of specifier names does not match the number of layers of folders in the fileDict")
     sTime = time()
-    numIms = _countIms(fileDict)
-    m = mp.Manager()
-    q = m.Queue()
-    thread = th.Thread(target = _loadIms, args=[q, fileDict, specifierNames])
-    thread.start()
-
-    if processorFunc is not None:
-        # Start processing
-        if parallel:
-            po = mp.Pool(processes = psutil.cpu_count(logical=False)-1)
-            cubes = po.starmap(processorFunc, [[q,*procArgs]]*numIms)
-        else:
-            cubes = [processorFunc(q,*procArgs) for i in range(numIms)]
+    files = _recursiveSearch(fileDict, specifierNames)
+    print(f'found {len(files)} files')
+#    numIms = len(files)
+    if parallel:
+        if processorFunc is None:
+            raise Exception("Running in parallel with no processorFunc is a bad idea.")
+        m = mp.Manager()
+        lock = m.Lock()
+        po = mp.Pool(processes = psutil.cpu_count(logical=False)-1)
+        cubes = po.starmap(loadThenProcess, zip(*zip(*[[processorFunc, procArgs, lock]]*len(files)), files))
     else:
-        cubes = [q.get() for i in range(numIms)]
-    thread.join()
+        qout = queue.Queue()
+        qin = queue.Queue()
+        [qin.put(f) for f in files]
+        lock = th.Lock()
+#        thread1 = th.Thread(target = _loadIms, args=[qout, qin, lock])
+#        thread2 = th.Thread(target = _loadIms, args=[qout, qin, lock])
+#        thread1.start()
+#        thread2.start()
+        threads = [th.Thread(target = _loadIms, args = [qout, qin, lock]) for i in range(numThreads)]
+        [thread.start() for thread in threads]
+        print('threads started')
+        if processorFunc is not None:
+            cubes = [processorFunc(qout.get(),*procArgs) for i in range(len(files))]
+        else:
+            cubes = [qout.get() for i in range(len(files))]
+#        thread1.join()
+#        thread2.join()
+        [thread.join() for thread in threads]
     print(f"Loading took {time()-sTime} seconds")
     return cubes
 
