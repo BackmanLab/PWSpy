908b7f18d2e2416ea66c5f6906ead201edd3917c
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-02-21 17:37:52 -0600

    add a plotting method to the positio list

--- src/pwspy/micromanagerPositions.py
+++ src/pwspy/micromanagerPositions.py
@@ -9,6 +9,8 @@ import json
 import typing
 import numpy as np
 import copy
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 class Property:
@@ -152,7 +154,42 @@ class PositionList:
         return len(self.positions)
     def __getitem__(self,idx:slice):
         return self.positions[idx]
-    
+    def plot(self):      
+        fig, ax = plt.subplots()
+        annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="w"),
+                    arrowprops=dict(arrowstyle="->"))
+        annot.set_visible(False)
+        ax.set_xlabel("x")
+        ax.set_ylabel('y')
+        cmap = mpl.cm.get_cmap("Spectral")
+        colors = [cmap(i) for i in np.linspace(0,1,num=len(self.positions))]
+        names = [pos.label for pos in self.positions]
+        sc = plt.scatter([pos.x for pos in self.positions], [pos.y for pos in self.positions], c = [colors[i] for i in range(len(self.positions))])
+        def update_annot(ind):
+            pos = sc.get_offsets()[ind["ind"][0]]
+            annot.xy = pos
+            text = "{}, {}".format(" ".join(list(map(str,ind["ind"]))), 
+                                   " ".join([names[n] for n in ind["ind"]]))
+            annot.set_text(text)
+#            annot.get_bbox_patch().set_facecolor(cmap(norm(c[ind["ind"][0]])))
+            annot.get_bbox_patch().set_alpha(0.4)
+            
+        def hover(event):
+            vis = annot.get_visible()
+            if event.inaxes == ax:
+                cont, ind = sc.contains(event)
+                if cont:
+                    update_annot(ind)
+                    annot.set_visible(True)
+                    fig.canvas.draw_idle()
+                else:
+                    if vis:
+                        annot.set_visible(False)
+                        fig.canvas.draw_idle()
+        
+        fig.canvas.mpl_connect("motion_notify_event", hover)
+        
 if __name__ == '__main__':
 
     def generateList(data:np.ndarray):
