0623521555c352966861f5760dce925248c30129
nanthony21 <nickmanthony@hotmail.com>
2019-02-27 20:59:11 -0600

    working on widgets. drawing is weird. check axeswidget

--- src/pwspy/imCube/matplotlibwidg.py
+++ src/pwspy/imCube/matplotlibwidg.py
@@ -13,7 +13,7 @@ from matplotlib import path
 import matplotlib.pyplot as plt
 from scipy import interpolate
 from shapely.geometry import LinearRing, Polygon as shapelyPolygon
-
+import time
 
 class myBase:
     def __init__(self, ax):
@@ -21,24 +21,36 @@ class myBase:
         self.canvas = ax.figure.canvas
         self.background = self.canvas.copy_from_bbox(self.ax.bbox)
         self.artists = []
+        self.connections = []
+        self.canvas.draw_idle()
 
     def update(self):
         self.canvas.restore_region(self.background)
-        [self.ax.draw_artist(i) for i in self.artists]
+        try:
+            [self.ax.draw_artist(i) for i in self.artists]
+        except Exception as e:
+            print(e)
         self.canvas.blit(self.ax.bbox)
         
     def setActive(self, active:bool):
         if active:
-            self.canvas.mpl_connect('draw_event', self.onDraw)
-            self.canvas.mpl_connect('button_press_event', self.onPress)
-            self.canvas.mpl_connect('key_press_event', self.onKey)
-            self.canvas.mpl_connect('button_release_event', self.onRelease)
-            self.canvas.mpl_connect('motion_notify_event', self.onMotion)
+            self.connections = []
+            self.connections.append(self.canvas.mpl_connect('draw_event', self.onDraw))
+            self.connections.append( self.canvas.mpl_connect('button_press_event', self._onPress))
+            self.connections.append(self.canvas.mpl_connect('key_press_event', self._onKey))
+            self.connections.append(self.canvas.mpl_connect('button_release_event', self._onRelease))
+            self.connections.append(self.canvas.mpl_connect('motion_notify_event', self._onMotion))
+        else:
+            [self.canvas.mpl_disconnect(i) for i in self.connections] 
+            self.connections = []
+        self.update()
+
+    def setVisible(self, visible:bool):
+        if visible:
             [i.set_visible(True) for i in self.artists]
         else:
-            for i in self.artists:
-                i.set_visible(False)         
-        self.update()
+            [i.set_visible(False) for i in self.artists]
+
 
     def onDraw(self, event):
         self.background = self.canvas.copy_from_bbox(self.ax.bbox)
@@ -46,7 +58,34 @@ class myBase:
         # do not need to blit here, this will fire before the screen is
         # updated
 
-class myLasso(_SelectorWidget):
+    def _onPress(self,event):
+        if event.inaxes==self.ax:
+            self.onPress(event)
+    
+    def _onKey(self,event):
+        self.onKey(event)
+    
+    def _onRelease(self,event):
+        if event.inaxes==self.ax:
+            self.onRelease(event)
+    
+    def _onMotion(self,event):
+        if event.inaxes==self.ax:
+            self.onMotion(event)
+    
+    def onPress(self,event):
+        pass
+    
+    def onKey(self,event):
+        pass
+    
+    def onRelease(self,event):
+        pass
+    
+    def onMotion(self,event):
+        pass
+
+class myLasso(myBase):
     """
     Selection curve of an arbitrary shape.
 
@@ -88,64 +127,41 @@ class myLasso(_SelectorWidget):
         - 3 = right mouse button
     """
 
-    def __init__(self, ax, onselect=None, useblit=True, button=None):
-        _SelectorWidget.__init__(self, ax, onselect, useblit=useblit, button=button)
+    def __init__(self, ax, onselect=None):
+        super().__init__(ax)
 
         self.verts = None
+        self.onselect = onselect
         self.polygon = Polygon([[0,0]], facecolor=(0,0,1,.1), animated=True, edgecolor=(0,0,1,.8))
         self.polygon.set_visible(False)
         self.ax.add_patch(self.polygon)
         self.artists = [self.polygon]
-        self.p = None
-        self.set_active(True) #needed for blitting to work
         
-    def _press(self, event):
-        if self.p is None:
-            self.verts = [self._get_data(event)]
-            self.set_visible(True)
+    def onPress(self, event):
+        self.verts = [[event.xdata, event.ydata]]
+        self.setVisible(True)
 
+    def onRelease(self, event):
+        if (self.verts is not None) and (self.onselect is not None):
+            self.onselect(self)#self.verts)
+#        self.setActive(False)
+#        l = shapelyPolygon(LinearRing(self.verts))
+#        l = l.buffer(0)
+#        l=l.simplify(l.length/2e2, preserve_topology=False)
+#        self.polygon.set_xy(l.exterior.coords)
+#        self.p=PolygonInteractor(self.ax, self.polygon.xy)
+#        self.p.setActive(True)
+#        self.setActive(True)
+#        self.verts = None
+#        self.update()
 
+    def onMotion(self, event):
+        if self.verts is None:
+            return
+        self.verts.append([event.xdata, event.ydata])
+        self.polygon.set_xy(self.verts)
+        self.update()
 
-    def _release(self, event):
-        if self.p is None:
-
-            self.disconnect_events()
-            if (self.verts is not None) and (self.onselect is not None):
-                self.onselect(self.verts)
-            self.set_visible(False)
-            self.set_active(False)
-    
-            l = shapelyPolygon(LinearRing(self.verts))
-            l = l.buffer(0)
-            l=l.simplify(l.length/2e2, preserve_topology=False)
-            self.polygon.set_xy(l.exterior.coords)
-    
-    #        plt.plot(*list(zip(*list(l.exterior.coords))))
-            
-    #        tck,u = interpolate.splprep([[i[0] for i in self.verts], [i[1] for i in self.verts]], per=True)
-    #        unew = np.linspace(0,1)
-    #        out = interpolate.splev(unew, tck)
-    #        self.ax.plot(out[0],out[1])
-    #        pat = path.Path(self.verts, closed=True)
-    #        newverts = [vert for vert,code in pat.iter_segments(simplify=True)]
-            self.p=PolygonInteractor(self.ax, self.polygon.xy)
-            self.p.setActive(True)
-    #        p.wait()
-            self.set_active(True)
-            self.connect_default_events()
-            self.verts = None
-            self.update()
-
-    def _onmove(self, event):
-        if self.p is None:
-            if self.verts is None:
-                return
-            self.verts.append(self._get_data(event))
-            self.polygon.set_xy(self.verts)
-            self.update()
-#            self.canvas.restore_region(self.background)
-#            self.ax.draw_artist(self.polygon)
-#            self.canvas.blit(self.ax.bbox)
         
         
 import numpy as np
@@ -174,27 +190,30 @@ class PolygonInteractor(myBase):
     showverts = True
     epsilon = 5  # max pixel distance to count as a vertex hit
 
-    def __init__(self, ax, verts):
+    def __init__(self, ax):
         super().__init__(ax)    
 
-        x, y = zip(*verts)
-        self.line = Line2D(x[:], y[:], ls="",
+        
+        self.line = Line2D([0], [0], ls="",
                            marker='o', markerfacecolor='r',
                            animated=True)
         self.ax.add_line(self.line)
-        self.artists.append(self.line)
         self._ind = None  # the active vert
         self._hoverInd = None
+ 
+        self.poly = Polygon([[0,0]], animated=True, facecolor=(0,0,1,.1), edgecolor=(0,0,1,.9))
+        self.ax.add_patch(self.poly)
+        self.artists = [self.poly, self.line]
+        [i.set_visible(False) for i in self.artists]
 
-        xy = self.interpolate()
-        self.line2 = Polygon(xy, animated=True, facecolor=(0,0,1,.1), edgecolor=(0,0,1,.9))
-        self.ax.add_patch(self.line2)
-        self.done = False
-        self.artists = [self.line2, self.line]
-        self.canvas.draw_idle()
-
-    def interpolate(self):
-        x,y = self.line.get_data()
+    def initialize(self, verts):
+        x, y = zip(*verts)
+        xy = self.interpolate(x, y)
+        self.line.set_data(x,y)
+        self.poly.set_xy(xy)
+        self.update()
+    def interpolate(self, x, y):
+#        x,y = self.line.get_data()
         tck, u = interpolate.splprep([x, y], s=0, per=True)   
         # evaluate the spline fits for 1000 evenly spaced distance values
         xi, yi = interpolate.splev(np.linspace(0, 1, 1000), tck)
@@ -279,7 +298,7 @@ class PolygonInteractor(myBase):
                 self.line.set_markerfacecolor((0,.9,1,1))
             else:
                 self.line.set_markerfacecolor('r')
-            self.update()
+            self.update()    
         if self._ind is None:
             return
 
@@ -293,8 +312,8 @@ class PolygonInteractor(myBase):
                 d[0] = (x, y)
             self.line.set_data(list(zip(*d)))
 
-            xy = self.interpolate()
-            self.line2.set_xy(xy)
+            xy = self.interpolate(*self.line.get_data())
+            self.poly.set_xy(xy)
             self.update()
             
         
@@ -303,4 +322,25 @@ if __name__ == '__main__':
     from pwspy import ImCube
 
     a = ImCube.loadAny(r'C:\Users\Nick\Downloads\lctf50msdelay\vf5nodelay\Cell1')
-    a.selectLassoROI()
+#    a.selectLassoROI()
+    fig, ax = a.plotMean()
+    fig.canvas.draw()
+    def onselect(ref):
+        ref.setActive(False)
+        l = shapelyPolygon(LinearRing(ref.verts))
+        l = l.buffer(0)
+        l=l.simplify(l.length/2e2, preserve_topology=False)
+#        self.polygon.set_xy(l.exterior.coords)
+#        print(list(l.exterior.coords))
+        p.initialize(l.exterior.coords)
+        p.setActive(True)
+        p.setVisible(True)
+#        ref.update()
+    
+    l = myLasso(ax, onselect=onselect)
+    p = PolygonInteractor(ax)
+
+#    l.setActive(False)
+    l.setActive(True)
+    fig.show()
+    
