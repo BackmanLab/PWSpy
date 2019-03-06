75180e7d13e7db0903b720a5af5ef9f34b08064d
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-03-05 18:00:37 -0600

    make cameraCorrection hashable

--- src/pwspy/imCube/otherClasses.py
+++ src/pwspy/imCube/otherClasses.py
@@ -9,8 +9,11 @@ import typing
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(frozen=True)
 class CameraCorrection:
     """linearityCorrection should be list of polynomial coefficients [a,b,c,etc...] in the order a*x + b*x^2 + c*x^3 + etc..."""
     darkCounts: float
-    linearityPolynomial: typing.List[float] = None
+    linearityPolynomial: typing.Tuple[float] = None
+
+    def __post_init__(self):
+        assert isinstance(self.linearityPolynomial, tuple)
