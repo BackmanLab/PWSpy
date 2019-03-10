a3473d11bc05a14604b0de7409e1a05316583c0e
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-03-10 12:16:43 -0500

    start adding functionality to extrareflection class

--- src/pwspy/imCube/ExtraReflectionCubeClass.py
+++ src/pwspy/imCube/ExtraReflectionCubeClass.py
@@ -1,5 +1,35 @@
-from pwspy.imCube.ICBaseClass import ICBase
+import json
+from typing import Tuple
 
+import h5py
+from pandas.tests.io.json import test_json_table_schema
+
+from pwspy.imCube.ICBaseClass import ICBase
+import numpy as np
+import os
 
 class ExtraReflectionCube(ICBase):
-    def __init__(self):
+    def __init__(self, data: np.ndarray, wavelengths:Tuple[float, ...], metadata: dict):
+        self._checkMetadata(metadata)
+        super().__init__(data, wavelengths)
+        self.metadata = metadata
+
+    @property
+    def wavelengths(self) -> Tuple[float, ...]:
+        return self.index
+
+    @classmethod
+    def load(cls, directory: str, name: str):
+
+
+    def save(self, directory: str, name: str) -> None:
+        savePath = os.path.join(directory, f'.h5')
+        if os.path.exists(savePath):
+            raise OSError(f"The path {savePath} already exists.")
+        with h5py.File(savePath, 'w') as hf:
+            hf.create_dataset('data', data=self.data)
+            hf.create_dataset('wavelengths', data=self.wavelengths)
+            hf.create_dataset('metadata', np.string_(json.dumps(self.metadata)))
+
+    def _checkMetadata(self, md: dict) -> None:
+        
