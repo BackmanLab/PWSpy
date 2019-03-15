c86cd30ac3736692dd515fad3857564654be9430
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-03-15 12:57:17 -0500

    add error logging

--- src/pwspy/gui/__main__.py
+++ src/pwspy/gui/__main__.py
@@ -1,3 +1,8 @@
+import os
+import traceback
+
+from . import applicationVars
+
 
 def isIpython():
     try:
@@ -10,12 +15,17 @@ def isIpython():
 if __name__ == '__main__':
     from pwspy.gui.analysis import PWSApp
     import sys
-
-    if isIpython():
-        app = PWSApp(sys.argv)
-    else:
-        print("Not Ipython")
-        # app = QApplication(sys.argv)
-        # ex = App()
-        app = PWSApp(sys.argv)
-        sys.exit(app.exec_())
+    try:
+        if isIpython():
+            app = PWSApp(sys.argv)
+        else:
+            print("Not Ipython")
+            # app = QApplication(sys.argv)
+            # ex = App()
+            app = PWSApp(sys.argv)
+            sys.exit(app.exec_())
+    except Exception as e:
+        with open(os.path.join(applicationVars.dataDirectory, 'crashLog.txt'), 'w') as f:
+            traceback.print_exc(limit=None, file=f)
+            print(f"Error Occurred: Please check {os.path.join(applicationVars.dataDirectory, 'crashLog.txt')}")
+        raise e
