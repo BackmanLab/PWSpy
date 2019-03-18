67108a3cd25f395885977bb7162fdc72a6b110a2
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-03-18 13:52:34 -0500

    get rid of problematic assertion

--- src/pwspy/imCube/otherClasses.py
+++ src/pwspy/imCube/otherClasses.py
@@ -25,10 +25,7 @@ class RoiFileFormats(Enum):
 class CameraCorrection:
     """linearityCorrection should be list of polynomial coefficients [a,b,c,etc...] in the order a*x + b*x^2 + c*x^3 + etc..."""
     darkCounts: float
-    linearityPolynomial: typing.Tuple[float, ...] = None
-
-    def __post_init__(self):
-        assert isinstance(self.linearityPolynomial, tuple)
+    linearityPolynomial: typing.Optional[typing.Tuple[float, ...]] = None
 
 
 class Roi:
