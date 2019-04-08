f4d3530a3328947f2dbba661b4a5a0cf58632b38
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-04-08 18:23:15 -0500

    update date comparison script.  needs test

--- src/pwspy/apps/ExtraReflectanceCreator/dateComparison.py
+++ src/pwspy/apps/ExtraReflectanceCreator/dateComparison.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 import os
 import random
 import matplotlib.animation as animation
-
+import pandas as pd
 
 
 def processIm(im):
@@ -31,32 +31,29 @@ if __name__ == '__main__':
     __spec__ = None
     materials = ['air','water', 'ipa','ethanol']
     rootDir = r'G:\Calibrations\4matExtraReflection\STORM'
+    settings = [os.path.split(f)[-1] for f in glob(os.path.join(rootDir, '*'))]
     folders = glob(os.path.join(rootDir,'*'))
     dates = [os.path.split(f)[-1] for f in folders]
-#    mat2Cell = {'air':[5,6],
-#                'water':[3,4],
-#                'ethanol':[1,2],
-#                'ipa':[7,8]}
-    mat2Cell = {'air':[6],
-                'water':[4],
-                'ethanol':[2],
-                'ipa':[8]}
-    
-    fileDict = {m:{folder:glob(os.path.join(rootDir, folder, f'Cell{mat2Cell[m]}')) for folder in dates} for m in materials}
-    cubes = loadAndProcess(fileDict, processIm, specifierNames = ['material', 'date'], parallel = True)
+
+    fileFrame = pd.DataFrame([{'setting': s, 'material': m, 'cube': cube} for s in settings for m in materials for cube in glob(os.path.join(rootDir,s, m, 'Cell*'))])
+    # fileDict = {m:{folder:glob(os.path.join(rootDir, folder, f'Cell{mat2Cell[m]}')) for folder in dates} for m in materials}
+    cubes = loadAndProcess(fileFrame, processIm, parallel = True)
     print("Select an ROI")
     mask = random.choice(cubes).selectLassoROI()
     anis=[]
     for mat in materials:
-        c = [im for im in cubes if im.material == mat]
+        c = cubes[cubes['material'] == mat]
+        # c = [im for im in cubes if im.material == mat]
         fig, ax = plt.subplots()
         fig.suptitle(mat)
         fig2, ax2 = plt.subplots()
         fig2.suptitle(mat)
         anims=[]
-        for im in c:
+        for i, row in c.iterrows():
+            im = row['cube']
             spectra = im.getMeanSpectra(mask)[0]
-            ax.plot(im.wavelengths, spectra, label=im.date)
-            anims.append((ax2.imshow(im.data.mean(axis=2), animated=True, clim=[np.percentile(im.data,.5),np.percentile(im.data,99.5)]), ax2.text(200,100,im.date)))
+            ax.plot(im.wavelengths, spectra, label=row['setting'])
+            anims.append((ax2.imshow(im.data.mean(axis=2), animated=True, clim=[np.percentile(im.data,.5),np.percentile(im.data,99.5)]), ax2.text(200,100,row['setting'])))
         ax.legend()
         anis.append(animation.ArtistAnimation(fig2, anims, interval=1000, blit=False))
+        
