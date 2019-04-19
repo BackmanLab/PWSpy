0694aa848308258d5313a91f62e4c9a9915016d3
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-04-19 15:18:13 -0500

    switch to different method.

--- src/pwspy/imCube/ICBaseClass.py
+++ src/pwspy/imCube/ICBaseClass.py
@@ -16,8 +16,8 @@ from matplotlib import path
 import typing, numbers
 
 from pwspy.imCube.otherClasses import Roi
-from skimage.feature import match_template
 from matplotlib import patches
+import cv2
 
 class ICBase:
     """A class to handle the data operations common to PWS related `image cubes`. Does not contain any file specific
@@ -221,33 +221,88 @@ class ICBase:
 
     def getTransform(self, other: Iterable['self.__class__'], debugPlots: bool = False) -> Iterable[Tuple[np.ndarray, float]]:
         midIdx = self.index[len(self.index)//2]
-        midPlane = self.selIndex(midIdx, midIdx)
+        midPlane = self.selIndex(midIdx, midIdx).data.squeeze()
 
-        regions = {}
-        for i in range(2): #TODO allow selection of more than two points. do this once better interactive roi drawing is in.
-            regions[i]={}
-            print("Template")
-            regions[i]['kernel'] = midPlane.selectRectangleROI()[1] #save the slice that selects the user-selected range.
-            print("Analysis")
-            regions[i]['analysis'] = midPlane.selectRectangleROI()[1]
-        strengths = [[], []]
-        matches = [[], []]
-        for i, region in regions.items():
-            ref = np.squeeze(midPlane[(*region['kernel'],)].data)
-            for cube in other:
-                midPlaneOther = cube.selIndex(midIdx, midIdx).data.squeeze()
-                an = midPlaneOther[(*region['analysis'],)]
-                p = patches.Rectangle((region['kernel'][1].start, region['kernel'][0].start),
-                                  region['kernel'][1].stop - region['kernel'][1].start,
-                                  region['kernel'][0].stop - region['kernel'][0].start, fill=False, color='y')
-                result = match_template(an, ref)
+        MIN_MATCH_COUNT = 10
+        FLANN_INDEX_KDTREE = 0
 
-                match = np.unravel_index(np.argmax(result), result.shape)
-                strengths[i].append(np.max(result))
-                matches[i].append(match)
-                if debugPlots:
-                    fig, axs = plt.subplots(1, 2)
-                    axs[0].imshow(midPlaneOther)
-                    axs[0].add_patch(p)
-                    axs[1].imshow(result)
-        return matches, strengths
+
+        # Initiate SIFT detector
+        sift = cv2.SIFT()
+        kp1, des1 = sift.detectAndCompute(midPlane, None)
+
+        for cube in other:
+            midPlaneOther = cube.selIndex(midIdx, midIdx).data.squeeze()
+
+            # find the keypoints and descriptors with SIFT
+            kp2, des2 = sift.detectAndCompute(midPlaneOther, None)
+
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+            search_params = dict(checks=50)
+
+            flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+            matches = flann.knnMatch(des1, des2, k=2)
+
+            # store all the good matches as per Lowe's ratio test.
+            good = []
+            for m, n in matches:
+                if m.distance < 0.7 * n.distance:
+                    good.append(m)
+
+            if len(good) > MIN_MATCH_COUNT:
+                src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+                dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+                M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+                matchesMask = mask.ravel().tolist()
+
+                h, w = midPlane.shape
+                pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+                dst = cv2.perspectiveTransform(pts, M)
+
+                img2 = cv2.polylines(midPlaneOther, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+
+            else:
+                print("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
+                matchesMask = None
+
+            draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                               singlePointColor=None,
+                               matchesMask=matchesMask,  # draw only inliers
+                               flags=2)
+
+            img3 = cv2.drawMatches(midPlane, kp1, img2, kp2, good, None, **draw_params)
+
+            plt.figure()
+            plt.imshow(img3, 'gray')
+            plt.show()
+
+        # regions = {}
+        # for i in range(2): #TODO allow selection of more than two points. do this once better interactive roi drawing is in.
+        #     regions[i]={}
+        #     print("Template")
+        #     regions[i]['kernel'] = midPlane.selectRectangleROI()[1] #save the slice that selects the user-selected range.
+        #     print("Analysis")
+        #     regions[i]['analysis'] = midPlane.selectRectangleROI()[1]
+        # strengths = [[], []]
+        # matches = [[], []]
+        # for i, region in regions.items():
+        #     ref = np.squeeze(midPlane[(*region['kernel'],)].data)
+        #     for cube in other:
+        #         midPlaneOther = cube.selIndex(midIdx, midIdx).data.squeeze()
+        #         an = midPlaneOther[(*region['analysis'],)]
+        #         p = patches.Rectangle((region['kernel'][1].start, region['kernel'][0].start),
+        #                           region['kernel'][1].stop - region['kernel'][1].start,
+        #                           region['kernel'][0].stop - region['kernel'][0].start, fill=False, color='y')
+        #         result = match_template(an, ref)
+        #
+        #         match = np.unravel_index(np.argmax(result), result.shape)
+        #         strengths[i].append(np.max(result))
+        #         matches[i].append(match)
+        #         if debugPlots:
+        #             fig, axs = plt.subplots(1, 2)
+        #             axs[0].imshow(midPlaneOther)
+        #             axs[0].add_patch(p)
+        #             axs[1].imshow(result)
+        # return matches, strengths
