a0c20de8225f403fbd0c95384fd1fbd5be468434
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-04-25 15:58:07 -0500

    start adding way to select analysis for plotting

--- src/pwspy/apps/PWSAnalysisApp/dockWidgets/PlottingDock/__init__.py
+++ src/pwspy/apps/PWSAnalysisApp/dockWidgets/PlottingDock/__init__.py
@@ -1,13 +1,15 @@
 from typing import List
 
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QDockWidget, QWidget, QHBoxLayout, QScrollArea, QVBoxLayout, QPushButton, QMessageBox
+from PyQt5.QtWidgets import QDockWidget, QWidget, QHBoxLayout, QScrollArea, QVBoxLayout, QPushButton, QMessageBox, \
+    QComboBox, QLabel
 
 from pwspy.apps.PWSAnalysisApp.dockWidgets import CellSelectorDock
 from pwspy.imCube.ICMetaDataClass import ICMetaData
 from .widgets import AspectRatioWidget, LittlePlot
-
+import os
 # TODO add blinded roi drawing
+
 
 class PlottingDock(QDockWidget):
     def __init__(self, cellSelectorTable: CellSelectorDock):
@@ -28,6 +30,8 @@ class PlottingDock(QDockWidget):
         buttons = QWidget()
         buttons.setLayout(QVBoxLayout())
         _ = buttons.layout().addWidget
+        self.anNameComboBox = QComboBox(self)
+        self.updateAnalysisNames()
         self.plotRMSButton = QPushButton("RMS")
         self.plotBFButton = QPushButton('BF')
         self.plot3dButton = QPushButton("3D")
@@ -35,6 +39,8 @@ class PlottingDock(QDockWidget):
         self.plotRMSButton.released.connect(self.plotRMS)
         self.clearButton.released.connect(self.clearPlots)
 
+        _(QLabel("Analysis Name"))
+        _(self.anNameComboBox)
         _(self.plotRMSButton)
         _(self.plotBFButton)
         _(self.plot3dButton)
@@ -42,6 +48,10 @@ class PlottingDock(QDockWidget):
         self._widget.layout().addWidget(plotScroll)
         self._widget.layout().addWidget(buttons)
         self.setWidget(self._widget)
+
+    def updateAnalysisNames(self):
+        self.anNameComboBox.clear()
+        self.anNameComboBox.addItems([])  # TODO determine which analyses are present
 
     def addPlot(self, plot):
         self.plots.append(plot)
@@ -61,10 +71,17 @@ class PlottingDock(QDockWidget):
             self.arController.aspect = 1 / len(self.plots)
 
     def plotRMS(self):
+        analysisName = self.anNameComboBox.currentText()
         cells: List[ICMetaData] = self.selector.getSelectedCellMetas()
         if len(cells) == 0:
             messageBox = QMessageBox(self)
             messageBox.information(self, "Oops!", "Please select the cells you would like to plot.")
             messageBox.setFixedSize(500, 200)
         for cell in cells:
-            self.addPlot(LittlePlot(cell.loadAnalysis(cell.getAnalyses()[0]).rms, cell)) #TODO this crashes if no analysis is present
+            try:
+                if analysisName in cell.getAnalyses():
+                    self.addPlot(LittlePlot(cell.loadAnalysis(analysisName).rms, cell))
+                else:
+                    print(f'{os.path.split(cell.filePath)[-1]} does not have a "{analysisName}" analysis.')
+            except KeyError:
+                print(f'{os.path.split(cell.filePath)[-1]} does not have an RMS field for analysis "{analysisName}"')
