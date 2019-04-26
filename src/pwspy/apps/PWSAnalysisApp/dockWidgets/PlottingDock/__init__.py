cd7981d546b4a99184e10bdf258fab94ca315d3a
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-04-26 16:33:36 -0500

    get missing import

--- src/pwspy/apps/PWSAnalysisApp/dockWidgets/PlottingDock/__init__.py
+++ src/pwspy/apps/PWSAnalysisApp/dockWidgets/PlottingDock/__init__.py
@@ -2,7 +2,7 @@ from typing import List
 
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QDockWidget, QWidget, QHBoxLayout, QScrollArea, QVBoxLayout, QPushButton, QMessageBox, \
-    QComboBox, QLabel, QLineEdit
+    QComboBox, QLabel, QLineEdit, QSizePolicy
 
 from pwspy.apps.PWSAnalysisApp.dockWidgets import CellSelectorDock
 from pwspy.imCube.ICMetaDataClass import ICMetaData
