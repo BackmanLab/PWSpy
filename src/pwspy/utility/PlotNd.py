22c312c0a94ba05817ca16457eb4a3da897e57a2
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-05-03 12:48:42 -0500

    big reorg to plot nd. can now add blitting?

--- src/pwspy/utility/PlotNd.py
+++ src/pwspy/utility/PlotNd.py
@@ -9,12 +9,81 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
 
+lw = 0.5
 
+class SidePlot:
+    def __init__(self, ax: plt.Axes, dimLength: int, vertical: bool):
+        self.vertical = vertical
+        self.dimLength = dimLength
+        self.ax = ax
+        self.range = (0, 1)
+        markerData = (self.range, (0, dimLength))
+        if self.vertical:
+            markerData = markerData[1] + markerData[0]
+        self.plot = ax.plot([0], [0])[0]
+        self.markerLine = ax.plot(*markerData, color='r', linewidth=lw)[0]
+
+
+    def setMarker(self, pos: float):
+        data = ((pos, pos), self.range)
+        if self.vertical:
+            data = (data[1],) + (data[0],)
+        self.markerLine.set_data(*data)
+
+    def setRange(self, Min, Max):
+        self.range = (Min, Max)
+        if self.vertical:
+            _ = self.ax.set_xlim
+        else:
+            _ = self.ax.set_ylim
+        _(*self.range)
+
+    def setData(self, data):
+        ind = np.arange(self.dimLength)
+        if self.vertical:
+            data = (data, ind)
+        else:
+            data = (ind, data)
+        self.plot.set_data(*data)
+
+
+class CenterPlot:
+    def __init__(self, ax: plt.Axes, shape):
+        self.ax = ax
+        self.shape = shape
+        self.vLine = ax.plot([100, 100], [0, shape[0]], 'r', linewidth=lw)[0]
+        self.hLine = ax.plot([0, shape[1]], [100, 100], 'r', linewidth=lw)[0]
+        self.im = ax.imshow(np.zeros(shape), aspect='auto')
+        ax.set_xlim((0, shape[1]))
+        ax.set_ylim((0, shape[0]))
+
+        self.range = (0, 1)
+
+    def setRange(self, Min, Max):
+        self.range = (Min, Max)
+        self.im.set_clim(*self.range)
+
+    def setMarker(self, x, y):
+        self.hLine.set_data([0, self.shape[1]], [y, y])
+        self.vLine.set_data([x, x], [0, self.shape[0]])
+
+    def setData(self, data):
+        self.im.set_data(data)
+
+class CBar:
+    def __init__(self, ax: plt.Axes, im):
+        self.ax = ax
+        self.cbar = plt.colorbar(im, cax=self.ax, orientation='horizontal')
+        self.ax.xaxis.set_ticks_position("top")
+
+
+#TODO use blitting
 class PlotNd(object):
     """A class to conveniently view 3d or greater data."""
     def __init__(self, X: np.ndarray, names=('y', 'x', 'lambda'), initialCoords=None, title=''):
         self.max = self.min = None
         fig = plt.figure(figsize=(6, 6))
+        self.fig = fig
         fig.suptitle(title)
         h, w = X.shape[:2]
         self.names = names
@@ -22,17 +91,28 @@ class PlotNd(object):
         gs = gridspec.GridSpec(3, 2 + self.extraDims + 1, hspace=0,
                                width_ratios=[w * .2 / (self.extraDims + 1)] * (self.extraDims + 1) + [w, w * .2],
                                height_ratios=[h * .1, h, h * .2], wspace=0)
-        imSubPlot = plt.subplot(gs[1, self.extraDims + 1])
-        ax = dict(im=imSubPlot,
-                  y=plt.subplot(gs[1, self.extraDims + 2], sharey=imSubPlot),
-                  x=plt.subplot(gs[2, self.extraDims + 1], sharex=imSubPlot),
-                  extra=[plt.subplot(gs[1, i]) for i in range(self.extraDims)],
-                  c=plt.subplot(gs[0, self.extraDims + 1]))
-        ax['y'].set_title(names[0])
-        ax['x'].set_xlabel(names[1])
-        ax['y'].yaxis.set_ticks_position('right')
-        [ax['extra'][i].set_ylim(0, X.shape[2 + i] - 1) for i in range(self.extraDims)]
-        [ax['extra'][i].set_title(names[2 + i]) for i in range(self.extraDims)]
+        ax: plt.Axes = plt.subplot(gs[1, self.extraDims + 1])
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        self.cp = CenterPlot(ax, X.shape[:2])
+
+        ax: plt.Axes = plt.subplot(gs[1, self.extraDims + 2], sharey=self.cp.ax)
+        ax.set_title(names[0])
+        ax.yaxis.set_ticks_position('right')
+        self.spY = SidePlot(ax, X.shape[0], True)
+
+        ax: plt.Axes = plt.subplot(gs[2, self.extraDims + 1], sharex=self.cp.ax)
+        ax.set_xlabel(names[1])
+        self.spX = SidePlot(ax, X.shape[1], False)
+
+        ax = plt.subplot(gs[0, self.extraDims + 1])
+        self.cbar = CBar(ax, self.cp.im)
+
+        extra = [plt.subplot(gs[1, i]) for i in range(self.extraDims)]
+        [extra[i].set_ylim(0, X.shape[2 + i] - 1) for i in range(self.extraDims)]
+        [extra[i].set_title(names[2 + i]) for i in range(self.extraDims)]
+        self.extra = [SidePlot(ax, X.shape[i+2], True) for i, ax in enumerate(extra)]
+
         fig.canvas.mpl_connect('key_press_event', self.onpress)
         fig.canvas.mpl_connect('scroll_event', self.onscroll)
         fig.canvas.mpl_connect('button_press_event', self.onclick)
@@ -40,66 +120,50 @@ class PlotNd(object):
         self.timer = fig.canvas.new_timer(interval=100)
         self.timer.add_callback(self.increment)
         self.timerRunning = False
-        self.ax = ax
         self.fig = fig
-        print(
-            '''Scroll to navigate stacks.\n\nPress "a" to automatically scroll.\n\nLeft click the color bar to set the max color range.\n\nRight click to set the mininum.\n\nPress "r" to reset the color range.\n\nPress 't' to swap the two primary axes.\n\nPress 'y' to rotate the order of the secondary axes.\n\nPress 'u' to rotate the order of all axes, allowing\n\a secondary axis to become a primary axis\n\n\n''')
+        '''Scroll to navigate stacks.\n\nPress "a" to automatically scroll.\n\nLeft click the color bar to set the max color range.\n\nRight click to set the mininum.\n\nPress "r" to reset the color range.\n\nPress 't' to swap the two primary axes.\n\nPress 'y' to rotate the order of the secondary axes.\n\nPress 'u' to rotate the order of all axes, allowing\n\a secondary axis to become a primary axis\n\n\n'''
         self.X = X
         self.resetColor()
 
         self.coords = tuple(i // 2 for i in X.shape) if initialCoords is None else initialCoords
-        self.ax['im'].get_xaxis().set_visible(False)
-        self.ax['im'].get_yaxis().set_visible(False)
-        lw = 0.5
-        self.vline = ax['im'].plot([100, 100], [0, X.shape[0]], 'r', linewidth=lw)[0]
-        self.hline = ax['im'].plot([0, X.shape[1]], [100, 100], 'r', linewidth=lw)[0]
-        self.line2 = ax['y'].plot([0, X.shape[1]], [100, 100], 'r', linewidth=lw)[0]
-        self.line3 = ax['x'].plot([100, 100], [0, X.shape[2]], 'r', linewidth=lw)[0]
-        self.line4 = [ax['extra'][i].plot([self.min, self.max], [100, 100], 'r', linewidth=lw)[0] for i in
-                      range(self.extraDims)]
-        self.yplot = ax['y'].plot(self.X[(slice(None),) + self.coords[1:]], np.arange(self.X.shape[0]))[0]
-        self.xplot = ax['x'].plot(np.arange(self.X.shape[1]), self.X[(self.coords[0], slice(None)) + self.coords[2:]])[
-            0]
-        self.zplots = [ax['extra'][i].plot(self.X[self.coords[:2 + i] + (slice(None),) + self.coords[3 + i:]],
-                                           np.arange(self.X.shape[2 + i]))[0] for i in range(self.extraDims)]
-        self.im = ax['im'].imshow(np.squeeze(self.X[(slice(None), slice(None)) + self.coords[2:]]), aspect='auto')
-        self.im.set_clim(self.min, self.max)
-        self.cbar = plt.colorbar(self.im, cax=ax['c'], orientation='horizontal')
+
+
         self.update()
+        self.updateLimits()
         plt.pause(0.1)
 
-    def save(self, path, interval = 50):
+    def save(self, path, interval=50):
         def f(self, z):
             self.coords = self.coords[:2] + (z,) + self.coords[3:]
             self.update()
 
-        ani = FuncAnimation(self.fig, lambda z: f(self, z), frames=list(range(self.X.shape[2])), blit=False,
-                            interval=interval)
+        ani = FuncAnimation(self.fig, lambda z: f(self, z), frames=list(range(self.X.shape[2])), blit=False, interval=interval)
         ani.save(path)
         return ani
 
     def update(self):
-        self.im.set_data(np.squeeze(self.X[(slice(None), slice(None)) + self.coords[2:]]))
-        self.im.set_clim(self.min, self.max)
-        self.yplot.set_data(self.X[(slice(None),) + tuple(i for i in self.coords[1:])], np.arange(self.X.shape[0]))
-        self.ax['y'].set_xlim(self.min, self.max)
-        self.xplot.set_data(np.arange(self.X.shape[1]), self.X[(self.coords[0], slice(None)) + self.coords[2:]])
-        self.ax['x'].set_ylim(self.min, self.max)
-        [self.zplots[i].set_data(self.X[self.coords[:2 + i] + (slice(None),) + self.coords[3 + i:]],
-                                 np.arange(self.X.shape[2 + i])) for i in range(self.extraDims)]
-        [self.ax['extra'][i].set_xlim(self.min, self.max) for i in range(self.extraDims)]
-        self.ax['c'].xaxis.set_ticks_position("top")
-        self.hline.set_data(self.hline.get_data()[0], [self.coords[0], self.coords[0]])
-        self.vline.set_data([self.coords[1], self.coords[1]], self.vline.get_data()[1])
-        self.line3.set_data([self.coords[1], self.coords[1]], self.ax['x'].get_ylim())
-        self.line2.set_data(self.ax['y'].get_xlim(), [self.coords[0], self.coords[0]])
-        [self.line4[i].set_data(self.line4[i].get_data()[0], [self.coords[2 + i], self.coords[2 + i]]) for i in
-         range(self.extraDims)]
-        self.im.axes.figure.canvas.draw()
+        self.cp.setData(np.squeeze(self.X[(slice(None), slice(None)) + self.coords[2:]]))
+        self.spY.setData(self.X[(slice(None),) + tuple(i for i in self.coords[1:])])
+        self.spX.setData(self.X[(self.coords[0], slice(None)) + self.coords[2:]])
+        for i, sp in enumerate(self.extra):
+            sp.setData(self.X[self.coords[:2 + i] + (slice(None),) + self.coords[3 + i:]])
+            sp.setMarker(self.coords[2 + i])
+        self.cp.setMarker(self.coords[1], self.coords[0])
+        self.spX.setMarker(self.coords[1])
+        self.spY.setMarker(self.coords[0])
+        self.fig.canvas.draw()
+
+    def updateLimits(self):
+        self.cp.setRange(self.min, self.max)
+        self.spY.setRange(self.min, self.max)
+        self.spX.setRange(self.min, self.max)
+        for sp in self.extra:
+            sp.setRange(self.min, self.max)
 
     def resetColor(self):
         self.max = np.percentile(self.X[np.logical_not(np.isnan(self.X))], 99.99)
         self.min = np.percentile(self.X[np.logical_not(np.isnan(self.X))], 0.01)
+        self.updateLimits()
 
     def onscroll(self, event):
         if (event.button == 'up') or (event.button == 'down'):
@@ -138,7 +202,6 @@ class PlotNd(object):
             PlotNd(newX, names, coords)
 
     def onclick(self, event):
-        #            print(event.button)
         if event.inaxes is None:
             return
         ax = event.inaxes
@@ -147,21 +210,23 @@ class PlotNd(object):
         self.processMouse(ax, x, y, button, colorbar=True)
 
     def processMouse(self, ax, x, y, button, colorbar):
-        if ax == self.ax['im']:
-            self.coords = (int(y), int(x)) + self.coords[2:]
-        elif ax == self.ax['y']:
-            self.coords = (int(y),) + self.coords[1:]
-        elif ax == self.ax['x']:
-            self.coords = (self.coords[0], int(x)) + self.coords[2:]
-        elif ax in self.ax['extra']:
-            idx = self.ax['extra'].index(ax)
-            self.coords = self.coords[:2 + idx] + (int(y),) + self.coords[3 + idx:]
         if colorbar:
-            if ax == self.ax['c']:
+            if ax == self.cbar.ax:
                 if button == 1:
                     self.max = x
                 elif button == 3:
                     self.min = x
+                self.updateLimits()
+                return
+        if ax == self.cp.ax:
+            self.coords = (int(y), int(x)) + self.coords[2:]
+        elif ax == self.spY.ax:
+            self.coords = (int(y),) + self.coords[1:]
+        elif ax == self.spX.ax:
+            self.coords = (self.coords[0], int(x)) + self.coords[2:]
+        elif ax in [sp.ax for sp in self.extra]:
+            idx = [sp.ax for sp in self.extra].index(ax)
+            self.coords = self.coords[:2 + idx] + (int(y),) + self.coords[3 + idx:]
         self.update()
 
     def ondrag(self, event):
@@ -189,7 +254,7 @@ if __name__ == '__main__':
     R = np.sqrt(X ** 2 + Y ** 2 + Z ** 2)
     A = np.exp(-R) * (.75 + .25 * np.sin(T))
     crop = np.sqrt(X ** 2 + Y ** 2) > .75
-
+    plt.interactive(True)
     '''We can also rotate the array if needed'''
     #    degrees = 35
     #    plane = (0,2) #rotating in the yz plane
@@ -199,3 +264,4 @@ if __name__ == '__main__':
     A[crop] = np.nan
 
     p = PlotNd(A, names)  # Input array dimensions should be [y,x,z]
+    plt.show(block=True)
