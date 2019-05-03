b3278f6e7f0a0870a652110ab59475e9d089aa4f
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-05-03 16:03:26 -0500

    Make cool adjustable plotter window. Use range slider from stack overflow

--- src/pwspy/apps/PWSAnalysisApp/dockWidgets/PlottingDock/widgets.py
+++ src/pwspy/apps/PWSAnalysisApp/dockWidgets/PlottingDock/widgets.py
@@ -3,16 +3,19 @@ from os import path as osp
 import numpy as np
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import QPoint
-from PyQt5.QtWidgets import QWidget, QBoxLayout, QSpacerItem, QGridLayout, QButtonGroup, QPushButton, QMenu, QAction
+from PyQt5.QtWidgets import QWidget, QBoxLayout, QSpacerItem, QGridLayout, QButtonGroup, QPushButton, QMenu, QAction, \
+    QSlider, QApplication, QLabel, QDialog, QVBoxLayout, QSpinBox, QDoubleSpinBox
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 from matplotlib.widgets import LassoSelector
 
 from pwspy.analysis.analysisResults import AnalysisResultsLoader
+from pwspy.apps.PWSAnalysisApp.sharedWidgets.rangeSlider import QRangeSlider
 from pwspy.imCube import ImCube
 from pwspy.imCube.ICMetaDataClass import ICMetaData
 from pwspy.utility import PlotNd
 from pwspy.utility.matplotlibwidg import MyLasso, AxManager, MyEllipse, mySelectorWidget, AdjustableSelector
+import matplotlib.pyplot as plt
 
 
 class AspectRatioWidget(QWidget):
@@ -52,6 +55,7 @@ class LittlePlot(FigureCanvasQTAgg, AnalysisPlotter):
         ax = self.fig.add_subplot(1, 1, 1)
         self.im = ax.imshow(np.zeros((100,100)))
         ax.set_title(title, fontsize=8)
+        self.title = title
         ax.yaxis.set_visible(False)
         ax.xaxis.set_visible(False)
         FigureCanvasQTAgg.__init__(self, self.fig)
@@ -64,7 +68,7 @@ class LittlePlot(FigureCanvasQTAgg, AnalysisPlotter):
 
     def mouseReleaseEvent(self, event):
         if event.button() == QtCore.Qt.LeftButton:
-            BigPlot(self.analysis, self.metadata, self)
+            BigPlot(self.data, self.title, self)
 
     def changeActiveAnalysisField(self, field):
         AnalysisPlotter.changeActiveAnalysisField(self, field)
@@ -90,6 +94,71 @@ class LittlePlot(FigureCanvasQTAgg, AnalysisPlotter):
 
 
 class BigPlot(AnalysisPlotter, QWidget):
+    class SaturationDialog(QDialog):
+        def __init__(self, parent):
+            super().__init__(parent=parent)
+            l = QVBoxLayout()
+            self.numBox = QDoubleSpinBox()
+            self.numBox.setValue(0.1)
+            self.numBox.setMinimum(0)
+            self.numBox.setSingleStep(0.1)
+            self.okButton = QPushButton("Ok")
+            self.okButton.released.connect(self.accept)
+            l.addWidget(QLabel("Saturation %"))
+            l.addWidget(self.numBox)
+            l.addWidget(self.okButton)
+            self.setLayout(l)
+
+        @property
+        def value(self):
+            return self.numBox.value()
+
+    def __init__(self,  data: np.ndarray, title: str, parent=None):
+        QWidget.__init__(self, parent=parent, flags=QtCore.Qt.Window)
+        self.setWindowTitle(title)
+        layout = QGridLayout()
+        self.data = data
+        self.fig = Figure()
+        self.ax = self.fig.add_subplot(1, 1, 1)
+        self.im = self.ax.imshow(data)
+        plt.colorbar(self.im, ax=self.ax)
+        self.canvas = FigureCanvasQTAgg(self.fig)
+        self.canvas.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.canvas.setFocus()
+        self.slider = QRangeSlider(self)
+        self.slider.endValueChanged.connect(self.climImage)
+        self.slider.startValueChanged.connect(self.climImage)
+        self.dlg = BigPlot.SaturationDialog(self)
+        self.dlg.accepted.connect(self.setSaturation)
+        self.saturationButton = QPushButton("Auto")
+        self.saturationButton.released.connect(self.dlg.show)
+        layout.addWidget(self.canvas, 0, 0, 8, 8)
+        layout.addWidget(QLabel("Color Range"), 9, 0, 1, 1)
+        layout.addWidget(self.slider, 9, 1, 1, 6)
+        layout.addWidget(self.saturationButton, 9, 7, 1, 1)
+        layout.addWidget(NavigationToolbar(self.canvas, self), 10, 0, 8, 8)
+        self.setLayout(layout)
+
+        M = self.data.max()
+        self.slider.setMax(M)
+        m = self.data.min()
+        self.slider.setMin(m)
+
+        self.show()
+        self.setSaturation()
+
+    def setSaturation(self):
+        percentage = self.dlg.value
+        m = np.percentile(self.data, percentage)
+        M = np.percentile(self.data, 100 - percentage)
+        self.slider.setStart(m)
+        self.slider.setEnd(M)
+
+    def climImage(self):
+        self.im.set_clim((self.slider.start(), self.slider.end()))
+        self.canvas.draw_idle()
+
+class RoiDrawer(AnalysisPlotter, QWidget):
     def __init__(self, analysis: AnalysisResultsLoader, metadata: ICMetaData, parent=None, initialField='rms'):
         QWidget.__init__(self, parent=parent, flags=QtCore.Qt.Window)
         AnalysisPlotter.__init__(self, analysis, metadata)
@@ -144,5 +213,7 @@ class BigPlot(AnalysisPlotter, QWidget):
         self.im.set_clim((np.percentile(self.data, 0.1), np.percentile(self.data, 99.9)))
         self.canvas.draw_idle()
 
-class RoiDrawer:
-    pass
+if __name__ == '__main__':
+    app = QApplication([])
+    b = BigPlot(np.random.random((1024, 1024)), 'Title')
+    app.exec()
