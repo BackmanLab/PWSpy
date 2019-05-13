783150d09bdc0f4da634433d54337049565b9409
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-05-13 10:34:40 -0500

    change cameraCorrection signature

--- src/pwspy/imCube/ImCubeClass.py
+++ src/pwspy/imCube/ImCubeClass.py
@@ -163,15 +163,16 @@ class ImCube(ICBase):
             raise Exception("The ImCube has already been normalized by exposure.")
         self._hasBeenNormalized = True
 
-    def correctCameraEffects(self, correction: CameraCorrection, binning: int = None):
+    def correctCameraEffects(self, correction: CameraCorrection = None, binning: int = None, auto: bool = False):
         """ Subtracts the darkcounts from the data. count is darkcounts per pixel. binning should be specified if it wasn't saved in the micromanager metadata."""
         if self._cameraCorrected:
             raise Exception("This ImCube has already had it's camera correction applied!")
-        if binning is None:
-            try:
-                binning = self.metadata.binning
-            except KeyError:
-                raise ValueError('Binning metadata not found. Binning must be specified in function argument.')
+        if auto:
+            assert (correction is None and binning is None), "correction and binning arguments should not be provided if auto is True"
+            binning = self.metadata.binning
+            correction = self.metadata.cameraCorrection
+            if binning is None: raise ValueError('Binning metadata not found. Binning must be specified in function argument.')
+            if correction is None: raise ValueError('CameraCorrection metadata not found. Binning must be specified in function argument.')
         count = correction.darkCounts * binning ** 2  # Account for the fact that binning multiplies the darkcount.
         self.data = self.data - count
         if correction.linearityPolynomial is None or correction.linearityPolynomial == (1.0,):
