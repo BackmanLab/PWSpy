4a0463be73bfe4655b459250aa8eba15a5a973b1
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-05-21 12:11:16 -0500

    attempt to free memory in analysis

--- src/pwspy/analysis/analysisClass.py
+++ src/pwspy/analysis/analysisClass.py
@@ -83,6 +83,7 @@ class LegacyAnalysis(AbstractAnalysis):
             imCubeIdTag=cube.metadata.idTag,
             referenceIdTag=self.ref.metadata.idTag,
             extraReflectionTag=None)
+        del reflectance, cube, rms, rmsPoly, slope, rSquared, ld #Delete these just in case they're hanging around.
         warns = [warn for warn in warns if warn is not None] #Filter out null values.
         return results, warns
 
