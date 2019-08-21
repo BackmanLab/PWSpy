77719321185c87a23b1897890f08ec7d6e045c1d
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-08-21 17:26:57 -0500

    fixed polygon adjuster point insertion

--- src/pwspy/utility/matplotlibwidg.py
+++ src/pwspy/utility/matplotlibwidg.py
@@ -464,6 +464,75 @@ class MyPaint(MySelectorWidget):
 
 
 
+
+
+
+
+def pnt2line(pnt, start, end):
+    import math
+    # Given a line with coordinates 'start' and 'end' and the
+    # coordinates of a point 'pnt' the proc returns the shortest
+    # distance from pnt to the line and the coordinates of the
+    # nearest point on the line.
+    #
+    # 1  Convert the line segment to a vector ('line_vec').
+    # 2  Create a vector connecting start to pnt ('pnt_vec').
+    # 3  Find the length of the line vector ('line_len').
+    # 4  Convert line_vec to a unit vector ('line_unitvec').
+    # 5  Scale pnt_vec by line_len ('pnt_vec_scaled').
+    # 6  Get the dot product of line_unitvec and pnt_vec_scaled ('t').
+    # 7  Ensure t is in the range 0 to 1.
+    # 8  Use t to get the nearest location on the line to the end
+    #    of vector pnt_vec_scaled ('nearest').
+    # 9  Calculate the distance from nearest to pnt_vec_scaled.
+    # 10 Translate nearest back to the start/end line.
+    # Malcolm Kesson 16 Dec 2012
+    def dot(v, w):
+        x, y, z = v
+        X, Y, Z = w
+        return x * X + y * Y + z * Z
+
+    def length(v):
+        x, y, z = v
+        return math.sqrt(x * x + y * y + z * z)
+
+    def vector(b, e):
+        x, y, z = b
+        X, Y, Z = e
+        return (X - x, Y - y, Z - z)
+
+    def unit(v):
+        x, y, z = v
+        mag = length(v)
+        return (x / mag, y / mag, z / mag)
+
+    def distance(p0, p1):
+        return length(vector(p0, p1))
+
+    def scale(v, sc):
+        x, y, z = v
+        return (x * sc, y * sc, z * sc)
+
+    def add(v, w):
+        x, y, z = v
+        X, Y, Z = w
+        return (x + X, y + Y, z + Z)
+    line_vec = vector(start, end)
+    pnt_vec = vector(start, pnt)
+    line_len = length(line_vec)
+    line_unitvec = unit(line_vec)
+    pnt_vec_scaled = scale(pnt_vec, 1.0/line_len)
+    t = dot(line_unitvec, pnt_vec_scaled)
+    if t < 0.0:
+        t = 0.0
+    elif t > 1.0:
+        t = 1.0
+    nearest = scale(line_vec, t)
+    dist = distance(nearest, pnt_vec)
+    nearest = add(nearest, start)
+    return (dist, nearest)
+
+
 class PolygonInteractor(MySelectorWidget):
     """
     A polygon editor.
@@ -503,6 +572,7 @@ class PolygonInteractor(MySelectorWidget):
         # evaluate the spline fits for 1000 evenly spaced distance values
         xi, yi = interpolate.splev(np.linspace(0, 1, 1000), tck)
         self.poly.set_xy(list(zip(xi, yi)))
+
 
     def _get_ind_under_point(self, event):
         """get the index of the vertex under point if within epsilon tolerance"""
@@ -546,18 +616,19 @@ class PolygonInteractor(MySelectorWidget):
                 self._interpolate()
         elif event.key == 'i':
             xys = list(self.markers.get_transform().transform(np.array(self.markers.get_data()).T))
-            p = np.array([event.x, event.y])  # display coords
+            p = np.array([event.x, event.y, 0])  # display coords
+            d = []
             for i in range(len(xys) - 1):
-                s0 = xys[i]
-                s1 = xys[i + 1]
-                d = np.linalg.norm(np.cross(s0-s1, s1-p))/np.linalg.norm(s0-s1) #distance from line to click point
-                if d <= self.epsilon:
-                    x, y = self.markers.get_data()
-                    self.markers.set_data(np.insert(x, i + 1, event.xdata), np.insert(y, i + 1, event.ydata))
-                    self._interpolate()
-                    print(f"Insert at {i+1}")
-                    break
-            print("No Insert")
+                s0 = (xys[i][0], xys[i][1], 0)
+                s1 = (xys[i + 1][0], xys[i+1][1], 0) #Convert to 3d points.
+                d.append(pnt2line(p, s0, s1)[0]) #distance from line to click point
+            d = np.array(d)
+            i = d.argmin()
+            if d.min() <= self.epsilon:
+                x, y = self.markers.get_data()
+                self.markers.set_data(np.insert(x, i+1, event.xdata), np.insert(y, i+1, event.ydata))
+                self._interpolate()
+                print(f"Insert at {i+1}")
         elif event.key == 'enter':
             self.onselect(self.poly.xy, self.markers.get_data())
             return
