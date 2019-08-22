d06f7a27f7525314b00090342e8592224548e10e
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-08-22 12:20:17 -0500

    fix install script bug

--- installScripts/install.bat
+++ installScripts/install.bat
@@ -6,10 +6,11 @@ call "%root%\Scripts\activate.bat" "%root%"
 
 call conda activate %env%
 
+set currDir=%cd%
+
 if errorlevel 1 (
 	echo Please create a Conda environment named %env%
 ) else (
-	set "currDir=%cd%"
 	call conda install -c file:///%currDir% -c defaults -c conda-forge pwspy --force-reinstall
 )
 
