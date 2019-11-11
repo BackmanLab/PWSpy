c64e5a962ad2e31f3606b73d39dab5744e92bc63
Nick Anthony <nicholas.anthony@northwestern.edu>
2019-11-11 15:19:38 -0600

    fix comments in install bat script. cant' share line with code.

--- installScripts/install.bat
+++ installScripts/install.bat
@@ -1,17 +1,22 @@
-set env=pwspy ::This should be the name of the Conda environment you want to install to. This environment needs to have already been created.
+:: This should be the name of the Conda environment you want to install to. This environment needs to have already been created.
+set env=pwspy
 
-set root=%userprofile%\Anaconda3 ::We assume anaconda is installed in the default location
+:: We assume anaconda is installed in the default location
+set root=%userprofile%\Anaconda3
 
-call "%root%\Scripts\activate.bat" "%root%" ::Call anaconda's script to activate the CMD environment
+:: Call anaconda's script to activate the CMD environment
+call "%root%\Scripts\activate.bat" "%root%"
 
-call conda activate %env% ::Activate the conda env.
+:: Activate the conda env.
+call conda activate %env%
 
 set currDir=%cd%
 
 if errorlevel 1 (
 	echo Please create a Conda environment named %env%
 ) else (
-	call conda install -c file:///%currDir% -c defaults -c conda-forge pwspy --force-reinstall ::Install the pwspy package from the current directory.
+:: Install the pwspy package from the current directory.
+	call conda install -c file:///%currDir% -c defaults -c conda-forge pwspy --force-reinstall
 )
 
 pause
