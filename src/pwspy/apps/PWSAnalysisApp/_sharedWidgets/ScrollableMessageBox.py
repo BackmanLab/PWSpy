7c97005a97a11d318fd34a863555401ed5b8ca59
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-01-07 14:30:18 -0600

    got scrollableMessageBox working

--- src/pwspy/apps/PWSAnalysisApp/_sharedWidgets/ScrollableMessageBox.py
+++ src/pwspy/apps/PWSAnalysisApp/_sharedWidgets/ScrollableMessageBox.py
@@ -6,25 +6,23 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMessageBox, QScrollArea, QWidget, QVBoxLayout, QLabel, QPushButton, QApplication
 import typing
 
+
 class ScrollableMessageBox(QMessageBox):
     def __init__(self, icon: QMessageBox.Icon, title: str, text: str, buttons: QMessageBox.StandardButtons = QMessageBox.NoButton, parent: QWidget = None,
                 f: QtCore.Qt.WindowFlags = QtCore.Qt.Dialog | QtCore.Qt.MSWindowsFixedSizeDialogHint):
         QMessageBox.__init__(self, icon, title, "", buttons, parent, f)
-        self.setFixedSize(512, 512)
         scroll = QScrollArea(self)
         scroll.setWidgetResizable(True)
         self.content = QWidget()
         lay = QVBoxLayout()
         self.content.setLayout(lay)
         scroll.setWidget(self.content)
+        scroll.setStyleSheet("QScrollArea{min-width:300 px; min-height: 200px}")
         label = QLabel(text, self)
         label.setWordWrap(True)
         lay.addWidget(label)
+        lay.setAlignment(QtCore.Qt.AlignTop)
         self.layout().replaceWidget(self.layout().itemAt(2).widget(), scroll)
-        # self.layout().addWidget(scroll, 0, 0, 1, self.layout().columnCount())
-        # self.layout().addWidget(scroll)
-
-        self.setStyleSheet("QScrollArea{min-width:300 px; min-height: 400px}")
 
     @staticmethod
     def question(parent: QWidget, title: str, text: str, buttons: typing.Union[QMessageBox.StandardButtons, QMessageBox.StandardButton] = QMessageBox.Yes|QMessageBox.No,
@@ -36,10 +34,10 @@ class ScrollableMessageBox(QMessageBox):
 
 if __name__ == "__main__":
     string = ""
-    for i in range(1000):
-        string+=str(i) + " "
+    for i in range(10):
+        string += str(i) + " "
     app = QApplication(sys.argv)
     button = QPushButton("GO")
-    scr = ScrollableMessageBox.question(button, "title", string)
+    scr = ScrollableMessageBox.question(button, "Test Title", string)
     print(scr)
     sys.exit(app.exec())
