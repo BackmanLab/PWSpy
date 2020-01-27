fix sign of dt. need to set invalid values to nan. setting to 0 lets them mess everything up.

src/pwspy/analysis/dynamics/_analysisClass.py
@@ -65,11 +65,11 @@ class DynamicsAnalysis(AbstractAnalysis):
         #Diffusion
         ac = cubeAc - self.refAc  # Background subtracted autocorrelation function
         ac = ac / ac[:, :, 0][:, :, None]  # Normalize by the zero-lag value
-        ac[ac <= 0] = 1e-323  # Before taking the log of the autocorrelation, zero values must be modified to prevent outputs of "inf" or "-inf".
+        ac[ac <= 0] = np.nan  # Before taking the log of the autocorrelation, zero values must be modified to prevent outputs of "inf" or "-inf".
         logac = np.log(ac)
 
-        dt = (cube.times[1] - cube.times[-1]) / (len(cube.time) - 1)
-        k = (self.n_medium * 2 * np.pi) / cube.metadata.wavelength
+        dt = (cube.times[-1] - cube.times[1]) / (len(cube.times) - 1) / 1e3  # Convert to seconds
+        k = (self.n_medium * 2 * np.pi) / (cube.metadata.wavelength / 1e3)  # expressing wavelength in microns to match up with old matlab code.
         val = logac / (dt * 4 * k ** 2)
         d_slope = -(val[:, :, 1] - val[:, :, 0]) #Get the slope
 