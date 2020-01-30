d1fc9b457d18cdb0f5aa79dab7872050233a6519
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-01-30 11:17:17 -0600

    start adding a new parameter to dynamics analysis

--- src/pwspy/analysis/dynamics/_analysisSettings.py
+++ src/pwspy/analysis/dynamics/_analysisSettings.py
@@ -6,12 +6,31 @@ from pwspy.moduleConsts import Material
 
 @dataclasses.dataclass
 class DynamicsAnalysisSettings(AbstractAnalysisSettings):
+    """These settings determine the behavior of the `DynamicsAnalysis` class.
+        Args:
+            extraReflectanceId: The unique `IDTag` of the extraReflectance calibration that was used on this analysis.
+            referenceMaterial: The material that was imaged in the reference image of this analysis. Found as an in pwspy.moduleConst.Material. The
+                theoretically predicted
+                reflectance of the reference image is used in the extraReflectance correction.
+            numericalAperture: The illumination NA of the system. This is used for two purposes. First, we want to make sure that the NA of our data matches
+                the NA of our extra reflectance correction cube.
+                Second, the theoretically predicted reflectance of our reference is based not only on what our refereMaterial is but also the NA since
+                reflectance is angle dependent.
+            relativeUnits: If `True` then all calculation are performed such that the reflectance is 1 if it matches the reference. If `False` then we use the
+                theoretical reflectance of the reference  (based on NA and reference material) to normalize our results to the actual physical reflectance of
+                the sample (about 0.4% for water)
+            diffusionRegressionLength: The original matlab scripts for analysis of dynamics data determined the slope of the log(ACF) by looking only at the
+                first two indices, (log(ACF)[1]-log(ACF)[0])/dt. This results in very noisy results. However as you at higher index value of the log(ACF) the
+                noise becomes much worse. A middle ground is to perform linear regression on the first 4 indices to determine the slope. You can adjust that
+                number here.
+    """
     extraReflectanceId: str
     referenceMaterial: Material
     numericalAperture: float
     relativeUnits: bool
+    diffusionRegressionLength: int
 
-    FileSuffix = "dynAnalysis"
+    FileSuffix = "dynAnalysis" # This is used to determine how to save
 
     def _asDict(self) -> dict:
         d = dataclasses.asdict(self)
