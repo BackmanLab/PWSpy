964e17cad3020ce73eb5390139e8c18993e78e00
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-02-18 22:14:32 -0600

    get rid of space between image and zplot

--- src/pwspy/utility/_PlotNd/_canvas.py
+++ src/pwspy/utility/_PlotNd/_canvas.py
@@ -27,28 +27,29 @@ class PlotNdCanvas(FigureCanvasQTAgg):
         extraDims = len(data.shape[2:])  # the first two axes are the image dimensions. Any axes after that are extra dimensions that can be scanned through
 
         h, w = data.shape[:2]
-        gs = gridspec.GridSpec(3, 2 + extraDims + 1, hspace=0,
-                               width_ratios=[.2 / (extraDims + 1)] * (extraDims + 1) + [1, .2],
+        gs = gridspec.GridSpec(3, 2 + extraDims, hspace=0,
+                               width_ratios=[.2 / (extraDims)] * extraDims + [1, .2],
                                height_ratios=[.1, 1, .2], wspace=0)
-        ax: plt.Axes = fig.add_subplot(gs[1, extraDims + 1])
+
+        ax: plt.Axes = fig.add_subplot(gs[1, extraDims])
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
         self.image = ImPlot(ax, data.shape[:2], (0, 1))
 
-        ax: plt.Axes = fig.add_subplot(gs[1, extraDims + 2], sharey=self.image.ax)
+        ax: plt.Axes = fig.add_subplot(gs[1, extraDims + 1], sharey=self.image.ax)
         ax.set_title(names[0])
         ax.yaxis.set_ticks_position('right')
         ax.get_xaxis().set_visible(False)
         self.spY = SidePlot(ax, data.shape[0], True, 0)
 
-        ax: plt.Axes = fig.add_subplot(gs[2, extraDims + 1], sharex=self.image.ax)
+        ax: plt.Axes = fig.add_subplot(gs[2, extraDims], sharex=self.image.ax)
         ax.set_xlabel(names[1])
         ax.xaxis.set_label_coords(.5, .95)
         ax.get_yaxis().set_visible(False)
         self.spX = SidePlot(ax, data.shape[1], False, 1)
         # self.spX.ax.set_xlabel(self.names[1])
 
-        ax: plt.Axes = fig.add_subplot(gs[0, extraDims + 1])
+        ax: plt.Axes = fig.add_subplot(gs[0, extraDims])
         self.cbar = CBar(ax, self.image.im)
 
         extra = [fig.add_subplot(gs[1, i]) for i in range(extraDims)]
