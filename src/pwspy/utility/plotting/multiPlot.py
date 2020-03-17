ffdfa427764326bd453bcbee5ed8571b5a365516
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-03-17 12:50:33 -0500

    add ability to save animation from multiplot

--- src/pwspy/utility/plotting/multiPlot.py
+++ src/pwspy/utility/plotting/multiPlot.py
@@ -1,7 +1,10 @@
+import os
+from enum import Enum, auto
 from typing import List
 
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QApplication
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QApplication, QDialog, QVBoxLayout, QHBoxLayout, QComboBox, QSpinBox, QLabel, QLineEdit, \
+    QFileDialog
 import numpy as np
 from matplotlib.artist import Artist
 from matplotlib.axes import Axes
@@ -10,6 +13,8 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from matplotlib.image import AxesImage
+from pwspy.apps import resources
+from matplotlib import animation
 
 
 class MultiPlot(QWidget):
@@ -33,10 +38,13 @@ class MultiPlot(QWidget):
         self.previousButton.released.connect(self.showPreviousIm)
         self.nextButton.released.connect(self.showNextIm)
 
+        self.saveButton = QPushButton("Save Animation")
+        self.saveButton.released.connect(lambda: AnimationDlg(self.figure, self.artists, self).exec())
 
         layout.addWidget(self.canvas, 0, 0, 8, 8)
         layout.addWidget(self.previousButton, 9, 1, 1, 1)
         layout.addWidget(self.nextButton, 9, 2, 1, 1)
+        layout.addWidget(self.saveButton, 9, 7, 1, 1)
         layout.addWidget(NavigationToolbar2QT(self.canvas, self), 10, 0, 1, 4)
 
         layout.setRowStretch(0, 1)  # This causes the plot to take up all the space that isn't needed by the other widgets.
@@ -44,7 +52,6 @@ class MultiPlot(QWidget):
 
         self.index = 0
         self._updateDisplayedImage()
-
 
     def showPreviousIm(self):
         self.index -= 1
@@ -69,6 +76,70 @@ class MultiPlot(QWidget):
                 artist.set_visible(self.index==i)
         self.canvas.draw_idle()
 
+
+class AnimationDlg(QDialog):
+    class SaveMethods(Enum):
+        GIF = 'pillow'
+        HTML = 'html'
+        MP4 = 'ffmpeg'
+
+    def __init__(self, fig, artists: List[List[Artist]], parent: QWidget):
+        super().__init__(parent)
+        self.setModal(True)
+        self.setWindowTitle("Save Animation")
+
+        self.artists = artists
+        self.figure = fig
+
+        self.intervalSpinBox = QSpinBox(self)
+        self.intervalSpinBox.setMinimum(0)
+        self.intervalSpinBox.setMaximum(10000)
+        self.intervalSpinBox.setSingleStep(50)
+        self.intervalSpinBox.setValue(100)
+
+        self.fPath = QLineEdit(self)
+        self.browseButton = QPushButton(QtGui.QIcon(os.path.join(resources, 'folder.svg')), '')
+        self.browseButton.released.connect(self.browseFile)
+
+
+        self.methodCombo = QComboBox(self)
+        [self.methodCombo.addItem(i.name, i) for i in self.SaveMethods]
+
+        self.saveButton = QPushButton("Save", self)
+        self.saveButton.released.connect(self.save)
+
+        layout = QVBoxLayout()
+        bottomLay = QHBoxLayout()
+
+        lay = QHBoxLayout()
+        lay.addWidget(QLabel("Frame Interval (ms):"))
+        lay.addWidget(self.intervalSpinBox)
+        layout.addLayout(lay)
+
+        lay = QHBoxLayout()
+        lay.addWidget(self.fPath)
+        lay.addWidget(self.browseButton)
+        layout.addLayout(lay)
+
+        bottomLay.addStretch()
+        bottomLay.addWidget(self.methodCombo)
+        bottomLay.addWidget(self.saveButton)
+        bottomLay.addStretch()
+        layout.addLayout(bottomLay)
+
+        self.setLayout(layout)
+
+    def save(self):
+        ani = animation.ArtistAnimation(self.figure, self.artists, interval=self.intervalSpinBox.value())
+        Writer = animation.writers[self.methodCombo.currentData().value]
+        writer = Writer()
+        ani.save(self.fPath.text(), writer=writer)
+        self.accept()
+
+    def browseFile(self):
+        fname, extension = QFileDialog.getSaveFileName(self, 'Save Location', os.getcwd())
+        if fname != '':
+            self.fPath.setText(fname)
 
 if __name__ == '__main__':
     import sys
