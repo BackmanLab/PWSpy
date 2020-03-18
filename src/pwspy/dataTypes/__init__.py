7f2ba13db2cd58af88a6a219e5defff0505b45a5
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-03-18 13:01:22 -0500

    improve docstring

--- src/pwspy/dataTypes/__init__.py
+++ src/pwspy/dataTypes/__init__.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug  7 13:20:25 2018
+Data Types Module
+==================
+This module contains all custom datatypes that are commonly used in the analysis of PWS related data.
 
-@author: Nick Anthony
+Metadata classes
+------------------
+These classes provide handling of information about an acquisition without requiring that the full data be loaded into RAM. These can be used to get information
+about the equipment used, the date the acquisition was taken, the location of the files, the presence of ROIs or analyses, etc.
+
+:class:`ICMetaData`:
+    Metadata for a PWS acquisition
+
+:class:`DynMetaData`:
+    Metadata for a dynamics acquisition
+
+:class:`ERMetadata`:
+    Metadata for an extra reflectance calibration file.
+
+TODO continue this.
 """
 import os
 _jsonSchemasPath = os.path.join(os.path.split(__file__)[0], 'jsonSchemas')
@@ -12,7 +28,8 @@ from ._FluoresenceImg import FluorescenceImage
 from ._arrayClasses import ImCube, DynCube, ExtraReflectionCube, ExtraReflectanceCube, KCube, ICBase, ICRawBase
 from ._AcqDir import AcqDir
 
-__all__ = ['ICBase', 'ICRawBase', 'ICMetaData', 'DynMetaData', 'ERMetadata', 'Roi', 'CameraCorrection', 'FluorescenceImage', 'ImCube', "DynCube", "ExtraReflectionCube", "ExtraReflectanceCube", 'KCube', 'AcqDir', 'FluorMetaData']
+__all__ = ['ICBase', 'ICRawBase', 'ICMetaData', 'DynMetaData', 'ERMetadata', 'Roi', 'CameraCorrection', 'FluorescenceImage', 'ImCube', "DynCube",
+           "ExtraReflectionCube", "ExtraReflectanceCube", 'KCube', 'AcqDir', 'FluorMetaData']
 
 
 
