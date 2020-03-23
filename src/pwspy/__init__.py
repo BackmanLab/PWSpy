a5829d4821813adced9c7595e872258475911b36
Nick <nickmanthony@hotmail.com>
2020-03-23 11:11:18 -0500

    running code now updates version file.

--- src/pwspy/__init__.py
+++ src/pwspy/__init__.py
@@ -7,5 +7,16 @@ Created on Tue Aug  7 13:20:25 2018
 import os
 
 __author__ = 'Nick Anthony'
+
+try:
+    import git
+    rootDir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    repo = git.Repo(rootDir)
+    version = repo.git.describe('--tags') #Get the output of the command `git describe --tags` serves as a good version number
+    with open(os.path.join(rootDir, 'src', 'pwspy', '_version'), 'w') as f: #Overwrite the version file
+        f.write(version)
+    print(f"Saved version, {version}, to the `_version` file.")
+except Exception as e:
+    pass
 with open(os.path.join(os.path.split(__file__)[0], '_version'), 'r') as f: # We load the version string from a text file. This allows us to easily set the contents of the text file with a build script.
     __version__ = str(f.readline())
