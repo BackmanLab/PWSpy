093ee5a9cf38ec8d2d490a7a7c28d842240d8897
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-03-25 20:37:20 -0500

    update datatype doc

--- docs/source/pwspy.dataTypes.rst
+++ docs/source/pwspy.dataTypes.rst
@@ -1,51 +1,6 @@
-pwspy.dataTypes package
-=======================
-.. py:currentmodule:: pwspy.dataTypes
-
 .. automodule:: pwspy.dataTypes
+   :no-members:
+   :no-inherited-members:
+   :no-special-members:
 
 
-Metadata Classes
-------------------
-These classes provide handling of information about an acquisition without requiring that the full data be loaded into RAM. These can be used to get information
-about the equipment used, the date the acquisition was taken, the location of the files, the presence of ROIs or analyses, etc.
-
-.. autosummary::
-    :toctree: generated/
-    :nosignatures:
-
-    ICMetaData
-    DynMetaData
-    ERMetaData
-
-Data Classes
------------------
-These classes are used to actuallly load and manipulate acquisition data. The all have a corresponding metadata class.
-
-.. autosummary::
-    :toctree: generated/
-    :nosignatures:
-
-    ImCube
-    DynCube
-    KCube
-    ExtraReflectanceCube
-    ExtraReflectionCube
-    ICBase
-    ICRawBase
-
-Other Classes
----------------
-.. autosummary::
-    :toctree: generated/
-    :nosignatures:
-
-    Roi
-    CameraCorrection
-    AcqDir
-    FluorescenceImage
-
-
-.. inheritance-diagram:: ImCube DynCube ICMetaData DynMetaData FluorescenceImage CameraCorrection Roi ERMetaData ExtraReflectionCube ExtraReflectanceCube KCube AcqDir FluorMetaData
-    :top-classes: ICBase, ICMetaData
-    :parts: 1
--- src/pwspy/dataTypes/__init__.py
+++ src/pwspy/dataTypes/__init__.py
@@ -1,8 +1,56 @@
 # -*- coding: utf-8 -*-
 """
-Data Types Module
-==========================
-This module contains all custom datatypes that are commonly used in the analysis of PWS related data.
+======================================
+Data Types (:mod:`pwspy.dataTypes`)
+======================================
+A module containing all custom datatypes that are commonly used in the analysis of PWS related data.
+
+
+Metadata Classes
+------------------
+These classes provide handling of information about an acquisition without requiring that the full data be loaded into RAM. These can be used to get information
+about the equipment used, the date the acquisition was taken, the location of the files, the presence of ROIs or analyses, etc.
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    ICMetaData
+    DynMetaData
+    ERMetaData
+
+Data Classes
+-----------------
+These classes are used to actuallly load and manipulate acquisition data. The all have a corresponding metadata class.
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    ImCube
+    DynCube
+    KCube
+    ExtraReflectanceCube
+    ExtraReflectionCube
+    ICBase
+    ICRawBase
+
+Other Classes
+---------------
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    Roi
+    CameraCorrection
+    AcqDir
+    FluorescenceImage
+
+Inheritance
+-------------
+.. inheritance-diagram:: ImCube DynCube ICMetaData DynMetaData FluorescenceImage CameraCorrection Roi ERMetaData ExtraReflectionCube ExtraReflectanceCube KCube AcqDir FluorMetaData
+    :top-classes: ICBase, ICMetaData
+    :parts: 1
 
 """
 
