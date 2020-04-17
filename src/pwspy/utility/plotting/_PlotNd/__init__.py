fix axis rotation and add QtConsole to PlotNd

src/pwspy/utility/plotting/_PlotNd/__init__.py
@@ -6,16 +6,28 @@ from PyQt5.QtWidgets import QWidget, QGridLayout, QApplication, QPushButton, QSp
     QGraphicsScene, QGroupBox, QVBoxLayout, QCheckBox, QButtonGroup
 from matplotlib import pyplot
 from matplotlib.animation import FuncAnimation
+from qtconsole.inprocess import QtInProcessKernelManager
+from qtconsole.jupyter_widget import JupyterWidget
+
 from pwspy.apps.sharedWidgets.rangeSlider import QRangeSlider
-from matplotlib.backends.backend_qt5 import NavigationToolbar2QT
+from matplotlib.backends.backend_qt5 import NavigationToolbar2QT, FigureCanvasQT
 import numpy as np
 from pwspy.utility.matplotlibWidgets import LassoSelector, PointSelector, AdjustableSelector
 from pwspy.utility.plotting._PlotNd._canvas import PlotNdCanvas
 from pwspy.utility.plotting._sharedWidgets import AnimationDlg
 
 
-class MyView(QGraphicsView):
-    def __init__(self, plot: PlotNdCanvas):
+class _MyView(QGraphicsView):
+    """This is a version of QGraphicsView that takes a Qt FigureCanvas from matplotlib and automatically resized the
+    canvas to fill as much of the view as possible. A debounce timer is used to prevent lag due to attempting the resize
+    the canvas too quickly. This allows for relatively smooth operation. This is essential for us to include a matplotlib
+    plot that can maintain it's aspect ratio within a Qt layout.
+
+    Args:
+        plot: A matplotlib FigureCanvas that is compatible with Qt (FigureCanvasQT or FigureCanvasQTAgg)
+
+    """
+    def __init__(self, plot: FigureCanvasQT):
         super().__init__()
         self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
@@ -24,26 +36,30 @@ class MyView(QGraphicsView):
         scene.addWidget(plot)
         self.plot = plot
         self.setScene(scene)
-        self.debounce = QTimer()
-        self.debounce.setSingleShot(True)
-        self.debounce.setInterval(50)
-        self.debounce.timeout.connect(self.resizePlot)
+        self._debounce = QTimer()  # If the resizeEvent doesn't reset the timer within the 50ms timeout interval then _resizePlot will be called.
+        self._debounce.setSingleShot(True)
+        self._debounce.setInterval(50)
+        self._debounce.timeout.connect(self._resizePlot)
 
-    def resizePlot(self):
-        w,h = self.size().width(), self.size().height()
+    def _resizePlot(self):
+        """This method is indirectly called by the resizeEvent through the debounce timer and sets the size of the plot
+        to maximize its size without changing aspect ratio."""
+        w, h = self.size().width(), self.size().height()
         r = self.scene().sceneRect()
-        s = min([w,h])
-        r.setSize(QSizeF(s,s))
-        self.plot.resize(s,s)
-        self.scene().setSceneRect(r)
+        s = min([w, h])  # Get the side length of the biggest square that can fit within the rectangle view area.
+        self.plot.resize(s, s)  # Set the plot to the size of the square that fits in view.
+        r.setSize(QSizeF(s, s))
+        self.scene().setSceneRect(r)  # Set the scene size to the square that fits in view.
 
     def resizeEvent(self, event: QtGui.QResizeEvent) -> None:
-        self.debounce.start()
+        """Every time that the view is resized this event will fire and start the debounce timer. The timer will only
+        actually time out if this event doesn't restart it within the timeout period."""
+        self._debounce.start()
         super().resizeEvent(event)
 
 
 class PlotNd(QWidget): #TODO Docstring
-    def __init__(self, data: np.ndarray, names: Tuple[str, ...] = ('y', 'x', 'lambda'),
+    def __init__(self, data: np.ndarray, names: Tuple[str, ...] = ('y', 'x', 'z'),
                  initialCoords: Tuple[int, ...] = None, title: str = '', parent: QWidget = None,
                  extraDimIndices: List[np.ndarray] = None):
         super().__init__(parent=parent)
@@ -54,8 +70,10 @@ class PlotNd(QWidget): #TODO Docstring
         if data.dtype == bool:
             data = data.astype(np.uint8)
 
+        self.console = None
+
         self.canvas = PlotNdCanvas(data, names, initialCoords, extraDimIndices)
-        self.view = MyView(self.canvas)
+        self.view = _MyView(self.canvas)
         self.slider = QRangeSlider(self)
         self.slider.setMaximumHeight(20)
         self.slider.setMax(np.nanmax(data))
@@ -93,6 +111,9 @@ class PlotNd(QWidget): #TODO Docstring
         self.noneButton.setChecked(True)
         self.buttonGroup.buttonReleased.connect(self.handleButtons)
 
+        self.consoleButton = QPushButton("Open Console")
+        self.consoleButton.released.connect(self.openConsole)
+
         self.rotateButton = QPushButton("Rotate Axes")
         self.rotateButton.released.connect(self.canvas.rollAxes)
 
@@ -103,8 +124,9 @@ class PlotNd(QWidget): #TODO Docstring
         layout = QGridLayout()
         layout.addWidget(self.view, 0, 0, 8, 8)
         layout.addWidget(self.buttonWidget, 0, 8, 4, 1)
-        layout.addWidget(self.rotateButton, 4, 8)
-        layout.addWidget(self.saveButton, 5, 8)
+        layout.addWidget(self.consoleButton, 4, 8)
+        layout.addWidget(self.rotateButton, 5, 8)
+        layout.addWidget(self.saveButton, 6, 8)
         layout.addWidget(NavigationToolbar2QT(self.canvas, self), 10, 0, 1, 8)
         layout.setRowStretch(0, 1)
         layout.addWidget(self.slider, 8, 0, 1, 7)
@@ -114,6 +136,35 @@ class PlotNd(QWidget): #TODO Docstring
 
         self.show()
         self.ar = self.height() / self.width()
+
+    def openConsole(self):
+        if self.console is None:
+            kernel_manager = QtInProcessKernelManager()
+            kernel_manager.start_kernel()
+            kernel = kernel_manager.kernel
+            kernel.gui = 'qt'
+            kernel.shell.push({'plot': self})
+
+            kernel_client = kernel_manager.client()
+            kernel_client.start_channels()
+
+            def stop():
+                kernel_client.stop_channels()
+                kernel_manager.shutdown_kernel()
+                app.exit()
+
+            self.console = JupyterWidget()
+            self.console.kernel_manager = kernel_manager
+            self.console.kernel_client = kernel_client
+        self.console.show()
+        msg = "'''plot: A reference to this PlotNd Object\nDocumentation here: https://pwspy.readthedocs.io/en/dev/generated/generated/generated/pwspy.utility.plotting.PlotNd.html#pwspy.utility.plotting.PlotNd'''"
+        self.console.do_execute(f"print('');print('');print({msg})", True, 0)
+        self.console.activateWindow()  # This should bring the window to the front
+
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        if self.console is not None:
+            self.console.close()
+        super().closeEvent(a0)
 
     def _updateLimits(self):
         self.canvas.updateLimits(self.slider.end(), self.slider.start())

src/pwspy/utility/plotting/_PlotNd/_canvas.py
@@ -19,6 +19,7 @@ def ifactive(func):
             return func(self, event)
     return newfunc
 
+
 class PlotNdCanvas(FigureCanvasQTAgg):
     def __init__(self, data: np.ndarray, names: Tuple[str, ...] = ('y', 'x', 'lambda'),
                  initialCoords: Tuple[int, ...] = None, extraDimIndices: typing.List = None):
@@ -145,7 +146,7 @@ class PlotNdCanvas(FigureCanvasQTAgg):
         for i in range(len(self.extra)):
             self.extra[i].ax.set_title(self.names[2+i])
 
-    def rollAxes(self): #TODO data of index and self.data are not aligned (reversed) after this operation
+    def rollAxes(self):
         self.setAxesNames([self.names[-1]] + list(self.names[:-1]))
         self._indexes = (self._indexes[-1],) + tuple(self._indexes[:-1])
         self.coords = (self.coords[-1],) + tuple(self.coords[:-1])