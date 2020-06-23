07c26ba13f89c430da8ab56f6f3b8472fba03d32
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2020-06-22 20:47:30 -0500

    progress

--- src/pwspy/apps/PWSAnalysisApp/plugins/acquisitionSequencer/Delegate.py
+++ src/pwspy/apps/PWSAnalysisApp/plugins/acquisitionSequencer/Delegate.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainter, QRegion
 from PyQt5.QtWidgets import QStyledItemDelegate, QWidget, QSpinBox, QStyleOptionViewItem, QStyle, QGridLayout, QLabel, \
     QTableWidget, QTableWidgetItem, QAbstractItemView, QScrollBar
@@ -9,30 +8,34 @@ import typing
 from .steps import SequencerStep, CoordSequencerStep, StepTypeNames
 
 
-class StepWidget(QWidget):
-    def __init__(self, step: SequencerStep):
-        super().__init__()
-        l = QGridLayout()
-        l.addWidget(QLabel(f"F: {str(step)}"))
-        self.setLayout(l)
+# class StepWidget(QWidget):
+#     def __init__(self, step: SequencerStep):
+#         super().__init__()
+#         l = QGridLayout()
+#         l.addWidget(QLabel(f"F: {str(step)}"))
+#         self.setLayout(l)
 
 
 class EditorWidg(QWidget):
     def __init__(self, parent: QWidget):
         super().__init__(parent)
+        self.setAutoFillBackground(True)  # Prevents us from getting double vision with the painted version of the widget behind.
         self._coordTable = QTableWidget(self)
         self._coordTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
         self._coordTable.setRowCount(1)
         self._coordTable.horizontalHeader().setVisible(False)
         self._coordTable.verticalHeader().setVisible(False)
-        self._coordTable.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-
+        self._coordTable.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)  # Always show scrollbar
+        self._coordTable.setHorizontalScrollMode(QAbstractItemView.ScrollPerPixel)  # Smooth scrolling
         self._label = QLabel(self)
 
+        self._requiredHeight = self._label.height() + self._coordTable.horizontalScrollBar().height() + self._coordTable.rowHeight(0)
+        self._coordTable.setFixedHeight(self._coordTable.horizontalScrollBar().height() + self._coordTable.rowHeight(0))
         l = QGridLayout()
-        l.addWidget(self._label)
-        l.addWidget(self._coordTable)
+        l.addWidget(self._label, 0, 0, 1, 1)
+        l.addWidget(self._coordTable, 1, 0, 1, 1, alignment=QtCore.Qt.AlignLeft)
+        # l.addWidget(QWidget(self), )
         self.setLayout(l)
 
     def setFromStep(self, step: CoordSequencerStep):
@@ -42,13 +45,15 @@ class EditorWidg(QWidget):
             self._coordTable.setItem(0, i, QTableWidgetItem(step.getIterationName(i)))
         self._coordTable.resizeColumnsToContents()
         self._coordTable.resizeRowsToContents()
+        w = 0
+        for i in range(step.stepIterations()):
+            w += self._coordTable.columnWidth(i)
+        self._coordTable.setMaximumWidth(w+5)
 
 
     def sizeHint(self) -> QtCore.QSize:
-        # return QtCore.QSize(600, 600)
-        # size = self.minimumSize()
+        # return QtCore.QSize(1, self._requiredHeight)  # Width doesn't matter here.
         return self.minimumSizeHint()
-        # return self.layout().sizeHint()
 
 
 class MyDelegate(QStyledItemDelegate):
@@ -60,6 +65,7 @@ class MyDelegate(QStyledItemDelegate):
         if isinstance(index.data(), CoordSequencerStep):
             widg = EditorWidg(parent)
             widg.setFromStep(index.data())
+            widg.resize(option.rect.size())
             return widg
         else:
             super().createEditor(parent, option, index)
@@ -80,12 +86,11 @@ class MyDelegate(QStyledItemDelegate):
         if isinstance(index.data(), CoordSequencerStep):
             if option.state & QStyle.State_Selected:
                 painter.fillRect(option.rect, option.palette.highlight())
-
             painter.save()
             self._paintWidget.setFromStep(index.data())
             self._paintWidget.resize(option.rect.size())
             painter.translate(option.rect.topLeft())
-            self._paintWidget.render(painter, QPoint(), QRegion(), QWidget.DrawChildren)
+            self._paintWidget.render(painter)#, None, None, None)#, QPoint(), QRegion(), QWidget.DrawChildren)
             painter.restore()
         else:
             super().paint(painter, option, index)
