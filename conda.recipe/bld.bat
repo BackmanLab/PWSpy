1bd4020107a1bd1a34078413c668f71b12cbf8ea
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2020-10-27 18:09:35 -0500

    Revert "add echo to build script to find issues."
    This reverts commit f2e18842ec89dda89be7b6d3ca7b927dbeb1b5fd.

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,17 +1,11 @@
-echo Start PWSPY Install
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
 
 if errorlevel 1 exit 1
 
-echo SUCCESS
-
 set MENU_DIR=%PREFIX%\Menu
-echo %MENU_DIR%
 IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 
 copy %SRC_DIR%\src\pwspy\apps\PWSAnalysisApp\_resources\cellLogo.ico %MENU_DIR%\
 if errorlevel 1 exit 1
 copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\pwspy_shortcut.json
 if errorlevel 1 exit 1
-
-echo SUCCESS
