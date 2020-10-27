3e1200462e22058cc92c5fdbae0dbe83d3dfb723
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2020-10-27 17:59:14 -0500

    fixes to linux build script

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -2,11 +2,12 @@
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
-MENU_DIR = $PREFIX\Menu
+MENU_DIR="$PREFIX\Menu"
 echo $MENU_DIR
-if [! -d "$MENU_DIR"]
+if [ ! -d "$MENU_DIR" ]
 then
   mkdir $MENU_DIR
+fi
 
 cp "$SRC_DIR\src\pwspy\apps\PWSAnalysisApp\_resources\cellLogo.ico" "$MENU_DIR"
 cp "$RECIPE_DIR\menu-windows.json" "$MENU_DIR\pwspy_shortcut.json"
