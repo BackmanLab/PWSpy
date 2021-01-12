2ac02d2429f6ecd8688ada86c1b5c967d087bc48
Nick Anthony <nicholas.anthony@northwestern.edu>
2021-01-12 13:17:19 -0600

    move our test loader to it's own file

---
+++ src/pwspy/apps/CalibrationSuite/testScripts/loader.py
@@ -0,0 +1,41 @@
+from pwspy.apps.CalibrationSuite.ITOMeasurement import ITOMeasurement
+from pwspy.apps.CalibrationSuite.loaders import AbstractMeasurementLoader
+import typing
+import pwspy.analysis.pws as pwsAnalysis
+import os
+import pwspy.dataTypes as pwsdt
+
+
+class Loader(AbstractMeasurementLoader):
+    """
+    An ITO calibration loader for this experiment. The reference for each ITO acquisiton is Cell3 from the same experiemental condition
+
+    """
+    analysisSettings = pwsAnalysis.PWSAnalysisSettings.loadDefaultSettings("Recommended")
+
+    def __init__(self, rootDir: str, measurementSetName: str):
+        meas = self.generateITOMeasurements(rootDir, measurementSetName)
+        template = [m for m in meas if m.name == 'centered_0_52'][0]
+        self._template = template
+        self._measurements = meas
+
+    @classmethod
+    def generateITOMeasurements(cls, rootDir: str, measurementSetName: str):
+        measurements = []
+        for expType in ['centered', 'fieldstop', 'translation']:
+            for condition in os.listdir(os.path.join(rootDir, expType)):
+                if os.path.isdir(os.path.join(rootDir, expType, condition)):
+                    itoAcq = pwsdt.AcqDir(os.path.join(rootDir, expType, condition, 'ito', 'Cell1'))
+                    refAcq = pwsdt.AcqDir(os.path.join(rootDir, expType, condition, 'cells', "Cell3"))
+                    name = f"{expType}_{condition}"
+                    homeDir = os.path.join(rootDir, "calibrationResults", measurementSetName, name)
+                    measurements.append(ITOMeasurement(homeDir, itoAcq, refAcq, cls.analysisSettings, name))
+        return measurements
+
+    @property
+    def template(self) -> ITOMeasurement:
+        return self._template
+
+    @property
+    def measurements(self) -> typing.Iterable[ITOMeasurement]:
+        return tuple(self._measurements)
