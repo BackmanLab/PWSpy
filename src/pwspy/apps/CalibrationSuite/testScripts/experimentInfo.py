d108c4638fc0ed405348f9d84e7aacfa66c713cc
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2021-01-15 01:51:21 -0600

    big improvements to plotting script

--- src/pwspy/apps/CalibrationSuite/testScripts/experimentInfo.py
+++ src/pwspy/apps/CalibrationSuite/testScripts/experimentInfo.py
@@ -4,20 +4,35 @@ Created on Tue Jan  5 19:24:03 2021
 
 @author: backman05
 """
+import pandas as pd
+
 workingDirectory = r'\\backmanlabnas.myqnapcloud.com\home\Year3\ITOPositionStability\ITOCalibrationSensitivity'
 
-experiment = {  # A dictionary of the three different detuning types that were done. The first item of each list should corresponde with a measurement at the default system settings (0.52NA and Field Stop open.)
+# A dictionary of the three different detuning types that were done.
+# The first item of each list should correspond with a measurement at
+# the default system settings (0.52NA and Field Stop open.)
+# Tuple of the form (settingName, settingQuantity)
+experiment = {
     'translation':
-        ['centered',
-         'decenter1',
-         'decenter2'],
+        [('centered', 0),
+         ('decenter1', 1),
+         ('decenter2', 2)],
     'centered':
-        ['0_52',
-         '0_39',
-         '0_50',
-         '0_55'],
+        [('0_52', .52),
+         ('0_39', .39),
+         ('0_50', .5),
+         ('0_55', .55)],
     'fieldstop':
-        ['open',
-         'barelyClosed',
-         'barelyClosed2']
+        [('open', 0),
+         ('barelyClosed', 1),
+         ('barelyClosed2', 2)]
     }
+
+l = []
+for expName, lst in experiment.items():
+    for idx, (setting, quant) in enumerate(lst):
+        l.append(
+            (expName, setting, quant, idx)
+        )
+experiment = pd.DataFrame(l, columns=['experiment', 'setting', 'settingQuantity', 'idx'])
+experiment['isref'] = experiment.idx == 0  # Index 0 measurements correspond to measurements that should have been set ot the correct settings (Field stop open, NA=0.52)
--- src/pwspy/apps/CalibrationSuite/testScripts/review.py
+++ src/pwspy/apps/CalibrationSuite/testScripts/review.py
@@ -14,7 +14,7 @@ import numpy as np
 def loadDataFrame(measurementSet: str) -> pd.DataFrame:
     loader = Loader(experimentInfo.workingDirectory, measurementSet)
     scoreNames = loader.measurements[0].loadTransformedData(
-        loader.template.idTag).getScoreNames()  # We assume that all measurement shave the same score names.
+        loader.template.idTag).getScoreNames()  # We assume that all measurements have the same score names.
 
     results = []
     for i in loader.measurements:
@@ -31,10 +31,10 @@ def loadDataFrame(measurementSet: str) -> pd.DataFrame:
             continue
         displacement = CVAffineTransform.fromPartialMatrix(result.affineTransform).translation
         displacement = np.sqrt(displacement[0] ** 2 + displacement[1] ** 2)
-        l.append({'displacement': displacement, 'name': measurement.name, 'measurement': measurement,
+        l.append({'displacement': displacement, 'measurementName': measurement.name, 'measurement': measurement,
                   'result': result})
 
-        scoreDict = {'name': measurement.name}
+        scoreDict = {'measurementName': measurement.name}
         for scoreName in scoreNames:
             score = result.getScore(scoreName)
             scoreDict[f"{scoreName}_score"] = score
@@ -43,7 +43,10 @@ def loadDataFrame(measurementSet: str) -> pd.DataFrame:
     df = pd.DataFrame(l)
     df2 = pd.DataFrame(l2)
 
-    df = pd.merge(df, df2, on='name')
+    df = pd.merge(df, df2, on='measurementName')
+    df['experiment'] = df.apply(lambda row: row['measurementName'].split('_')[0], axis=1)
+    df['setting'] = df.apply(lambda row: '_'.join(row['measurementName'].split('_')[1:]), axis=1)
+    del df['measurementName']
     return df
 
 
@@ -52,73 +55,218 @@ if __name__ == '__main__':
     plt.ion()
 
     #TODO split the SSIM
-    measurementSet = 'xcorr_blurScan2'
+    measurementSet = 'xcorr_blurScan'
 
     df = loadDataFrame(measurementSet)
+    df = pd.merge(df, experimentInfo.experiment, on=('experiment', 'setting'))
+    df['setting'] = df['setting'].apply(lambda val: val.replace('_', '.'))  # Replace the underscores with period to make the NA look better in plots
 
-
-    df['exp'] = df.apply(lambda row: row['name'].split('_')[0], axis=1)
-    df['setting'] = df.apply(lambda row: '_'.join(row['name'].split('_')[1:]), axis=1)
     scoreNames = [colName.split('_')[0] for colName in df.columns if colName.endswith("_score")]  # Assumes there is no '_' in the score name
+    #Sort scorenames numerically
+    scoreNameInts = [0 if x=='None' else int(x) for x in scoreNames]
+    scoreNameInts, scoreNames = list(zip(*sorted(zip(scoreNameInts, scoreNames))))
 
-    # Generate a column indicating for each experiment which order they should be plotted in based on the contents of the experimentInfo dictionary
-    a = df.groupby('exp', as_index=False).apply(
-        lambda g: g.apply(
-            lambda row: experimentInfo.experiment[g.exp.iloc[0]].index(row.setting), axis=1)
-        )
-    a.index = a.index.get_level_values(1) # Get the original index back
-    df['idx'] = a
+    scoreCmap = mpl.cm.nipy_spectral
+    scoreColors = scoreCmap(np.linspace(0, 0.95, num=len(scoreNames))) # Clip the ends to avoid the black and white at the ends of the nipy_specal cmap.
 
-    # Plot MSE Score
-    for expName, g in df.groupby('exp'):
+    def drawBlurCBar():
+        cbar = plt.colorbar(
+            mappable=mpl.cm.ScalarMappable(
+                norm=mpl.colors.Normalize(min(scoreNameInts), max(scoreNameInts)),
+                cmap=scoreCmap))
+        cbar.set_label("Blur (px)")
+
+    # Plot NRMSE Score
+    for expName, g in df.groupby('experiment'):
         fig, ax = plt.subplots()
-        fig.suptitle(f"MSE {expName}")
+        fig.suptitle(f"NRMSE {expName}")
         g = g.sort_values('idx')
-        colors = mpl.cm.nipy_spectral(np.linspace(0, 1, num=len(scoreNames)))
-        for scoreName, color in zip(scoreNames, colors):
+        for scoreName, color in zip(scoreNames, scoreColors):
             scores = g[f"{scoreName}_score"]
             scores = [i.nrmse.score for i in scores]
-            ax.scatter(g.idx, scores, label=scoreName, color=color)
-        plt.xticks(ticks=g.idx, labels=g.setting, rotation=20)
-        ax.legend()
+            ax.scatter(g.settingQuantity, scores, label=scoreName, color=color)
+        plt.xticks(ticks=g.settingQuantity, labels=g.setting, rotation=20)
+        drawBlurCBar()
 
     # Plot SSIM Score
-    for expName, g in df.groupby('exp'):
+    for expName, g in df.groupby('experiment'):
         fig, ax = plt.subplots()
         fig.suptitle(f"SSIM {expName}")
         g = g.sort_values('idx')
-        colors = mpl.cm.nipy_spectral(np.linspace(0, 1, num=len(scoreNames)))
-        for scoreName, color in zip(scoreNames, colors):
+        for scoreName, color in zip(scoreNames, scoreColors):
             scores = g[f"{scoreName}_score"]
             scores = [i.ssim.score for i in scores]
-            ax.scatter(g.idx, scores, label=scoreName, color=color)
-        plt.xticks(ticks=g.idx, labels=g.setting, rotation=20)
-        ax.legend()
+            ax.scatter(g.settingQuantity, scores, label=scoreName, color=color)
+        plt.xticks(ticks=g.settingQuantity, labels=g.setting, rotation=20)
+        drawBlurCBar()
 
     # Plot LatXCorr Score
-    for expName, g in df.groupby('exp'):
+    for expName, g in df.groupby('experiment'):
         fig, ax = plt.subplots()
         fig.suptitle(f"LatXCorr {expName}")
         g = g.sort_values('idx')
-        colors = mpl.cm.nipy_spectral(np.linspace(0, 1, num=len(scoreNames)))
-        for scoreName, color in zip(scoreNames, colors):
+        for scoreName, color in zip(scoreNames, scoreColors):
             scores = g[f"{scoreName}_score"]
             scores = [i.latxcorr.score for i in scores]
-            ax.scatter(g.idx, scores, label=scoreName, color=color)
-        plt.xticks(ticks=g.idx, labels=g.setting, rotation=20)
-        ax.legend()
+            ax.scatter(g.settingQuantity, scores, label=scoreName, color=color)
+        plt.xticks(ticks=g.settingQuantity, labels=g.setting, rotation=20)
+        drawBlurCBar()
 
     # Plot AxXCorr Score
-    for expName, g in df.groupby('exp'):
+    for expName, g in df.groupby('experiment'):
         fig, ax = plt.subplots()
         fig.suptitle(f"AxXCorr {expName}")
         g = g.sort_values('idx')
-        colors = mpl.cm.nipy_spectral(np.linspace(0, 1, num=len(scoreNames)))
-        for scoreName, color in zip(scoreNames, colors):
+        for scoreName, color in zip(scoreNames, scoreColors):
             scores = g[f"{scoreName}_score"]
             scores = [i.axxcorr.score for i in scores]
-            ax.scatter(g.idx, scores, label=scoreName, color=color)
-        plt.xticks(ticks=g.idx, labels=g.setting, rotation=20)
-        ax.legend()
+            ax.scatter(g.settingQuantity, scores, label=scoreName, color=color)
+        plt.xticks(ticks=g.settingQuantity, labels=g.setting, rotation=20)
+        drawBlurCBar()
+
+    for expName, g in df.groupby('experiment'):
+        fig, ax = plt.subplots()
+        fig.suptitle(f"Axial Shift: {expName}")
+        g = g.sort_values('idx')
+        for scoreName, color in zip(scoreNames, scoreColors):
+            scores = g[f"{scoreName}_score"]
+            scores = [i.axxcorr.shift for i in scores]
+            ax.scatter(g.settingQuantity, scores, label=scoreName, color=color)
+        plt.xticks(ticks=g.settingQuantity, labels=g.setting, rotation=20)
+        drawBlurCBar()
+
+    for expName, g in df.groupby('experiment'):
+        fig, ax = plt.subplots()
+        fig.suptitle(f"Axial CDR: {expName}")
+        g = g.sort_values('idx')
+        for scoreName, color in zip(scoreNames, scoreColors):
+            scores = g[f"{scoreName}_score"]
+            scores = [i.axxcorr.cdr for i in scores]
+            ax.scatter(g.settingQuantity, scores, label=scoreName, color=color)
+        plt.xticks(ticks=g.settingQuantity, labels=g.setting, rotation=20)
+        drawBlurCBar()
+
+    for expName, g in df.groupby('experiment'):
+        fig, ax = plt.subplots()
+        fig.suptitle(f"Lateral CDR_x (Normalized): {expName}")
+        g = g.sort_values('idx')
+        for scoreName, color in zip(scoreNames, scoreColors):
+            scores = g[f"{scoreName}_score"]
+            scores = np.array([i.latxcorr.cdrX for i in scores])
+            scores = scores / scores[0] # normalize by first value
+            ax.scatter(g.settingQuantity, scores, label=scoreName, color=color)
+        plt.xticks(ticks=g.settingQuantity, labels=g.setting, rotation=20)
+        drawBlurCBar()
+
+    for expName, g in df.groupby('experiment'):
+        fig, ax = plt.subplots()
+        fig.suptitle(f"Lateral CDR_y (Normalized): {expName}")
+        g = g.sort_values('idx')
+        for scoreName, color in zip(scoreNames, scoreColors):
+            scores = g[f"{scoreName}_score"]
+            scores = np.array([i.latxcorr.cdrY for i in scores])
+            scores = scores / scores[0] # normalize by first value
+            ax.scatter(g.settingQuantity, scores, label=scoreName, color=color)
+        plt.xticks(ticks=g.settingQuantity, labels=g.setting, rotation=20)
+        drawBlurCBar()
+
+    for expName, g in df.groupby('experiment'):
+        fig, ax = plt.subplots()
+        fig.suptitle(f"Lateral Shift: {expName}")
+        g = g.sort_values('idx')
+        for scoreName, color in zip(scoreNames, scoreColors):
+            scores = g[f"{scoreName}_score"]
+            scores = [np.sqrt(np.sum(np.array(i.latxcorr.shift)**2)) for i in scores]
+            ax.scatter(g.settingQuantity, scores, label=scoreName, color=color)
+        plt.xticks(ticks=g.settingQuantity, labels=g.setting, rotation=20)
+        drawBlurCBar()
+
+    ### Plots comparing the aligned images to eachother ###
+    g = df[df.isref]
+    g.index = list(range(len(g)))
+    fig, ax = plt.subplots()
+    fig.suptitle("Axial XCorr: Correctly Aligned")
+    for scoreName, color in zip(scoreNames, scoreColors):
+        scores = g[f"{scoreName}_score"]
+        scores = [i.axxcorr.score for i in scores]
+        ax.scatter(g.index, scores, label=scoreName, color=color)
+    plt.xticks(ticks=g.index, labels=g.experiment, rotation=20)
+    drawBlurCBar()
+
+    g = df[df.isref]
+    g.index = list(range(len(g)))
+    fig, ax = plt.subplots()
+    fig.suptitle("Lateral XCorr: Correctly Aligned")
+    for scoreName, color in zip(scoreNames, scoreColors):
+        scores = g[f"{scoreName}_score"]
+        scores = [i.latxcorr.score for i in scores]
+        ax.scatter(g.index, scores, label=scoreName, color=color)
+    plt.xticks(ticks=g.index, labels=g.experiment, rotation=20)
+    drawBlurCBar()
+
+    g = df[df.isref]
+    g.index = list(range(len(g)))
+    fig, ax = plt.subplots()
+    fig.suptitle("NRMSE: Correctly Aligned")
+    for scoreName, color in zip(scoreNames, scoreColors):
+        scores = g[f"{scoreName}_score"]
+        scores = [i.nrmse.score for i in scores]
+        ax.scatter(g.index, scores, label=scoreName, color=color)
+    plt.xticks(ticks=g.index, labels=g.experiment, rotation=20)
+
+    g = df[df.isref]
+    g.index = list(range(len(g)))
+    fig, ax = plt.subplots()
+    fig.suptitle("SSIM: Correctly Aligned")
+    for scoreName, color in zip(scoreNames, scoreColors):
+        scores = g[f"{scoreName}_score"]
+        scores = [i.ssim.score for i in scores]
+        ax.scatter(g.index, scores, label=scoreName, color=color)
+    plt.xticks(ticks=g.index, labels=g.experiment, rotation=20)
+    drawBlurCBar()
+
+    g = df[df.isref]
+    g.index = list(range(len(g)))
+    fig, ax = plt.subplots()
+    fig.suptitle("Lateral CDR_x: Correctly Aligned")
+    for scoreName, color in zip(scoreNames, scoreColors):
+        scores = g[f"{scoreName}_score"]
+        scores = [i.latxcorr.cdrX for i in scores]
+        ax.scatter(g.index, scores, label=scoreName, color=color)
+    plt.xticks(ticks=g.index, labels=g.experiment, rotation=20)
+    drawBlurCBar()
+
+    g = df[df.isref]
+    g.index = list(range(len(g)))
+    fig, ax = plt.subplots()
+    fig.suptitle("Lateral CDR_y: Correctly Aligned")
+    for scoreName, color in zip(scoreNames, scoreColors):
+        scores = g[f"{scoreName}_score"]
+        scores = [i.latxcorr.cdrY for i in scores]
+        ax.scatter(g.index, scores, label=scoreName, color=color)
+    plt.xticks(ticks=g.index, labels=g.experiment, rotation=20)
+    drawBlurCBar()
+
+    g = df[df.isref]
+    g.index = list(range(len(g)))
+    fig, ax = plt.subplots()
+    fig.suptitle("Lateral Shift: Correctly Aligned")
+    for scoreName, color in zip(scoreNames, scoreColors):
+        scores = g[f"{scoreName}_score"]
+        scores = [np.sqrt(np.sum(np.array(i.latxcorr.shift)**2)/2) for i in scores]
+        ax.scatter(g.index, scores, label=scoreName, color=color)
+    plt.xticks(ticks=g.index, labels=g.experiment, rotation=20)
+    drawBlurCBar()
+
+    g = df[df.isref]
+    g.index = list(range(len(g)))
+    fig, ax = plt.subplots()
+    fig.suptitle("Axial Shift: Correctly Aligned")
+    for scoreName, color in zip(scoreNames, scoreColors):
+        scores = g[f"{scoreName}_score"]
+        scores = [i.axxcorr.shift for i in scores]
+        ax.scatter(g.index, scores, label=scoreName, color=color)
+    plt.xticks(ticks=g.index, labels=g.experiment, rotation=20)
+    drawBlurCBar()
 
     a = 1
