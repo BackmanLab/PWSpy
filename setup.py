4fdbdbc2f45eaa259c8955850074c8818fca918d
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2021-01-21 12:37:27 -0600

    switch from gitpython to setuptools_scm

--- setup.py
+++ setup.py
@@ -21,17 +21,11 @@ This file is used to install the pwspy package. for example navigate in your ter
 file and type `pip install .`. This file is also used by the Conda recipe (buildscripts/conda)
 """
 from setuptools import setup, find_packages
-import os.path as osp
-import os
+import setuptools_scm
 
-with open(osp.join('src', 'pwspy', 'version.py')) as f:
-    currwd = os.getcwd()
-    os.chdir(osp.join(currwd, 'src', 'pwspy'))  # The version.py file will run from the wrong location if we don't manually set it here.
-    exec(f.read())  # Run version py to initialize the pwspyVersion variable
-    os.chdir(currwd)
 
 setup(name='pwspy',
-      version=pwspyVersion,
+      version=setuptools_scm.get_version(write_to="src/pwspy/version.py"),
       description='A framework for working with Partial Wave Spectroscopy files.',
       author='Nick Anthony',
       author_email='nicholas.anthony@northwestern.edu',
@@ -46,24 +40,14 @@ setup(name='pwspy',
                         'pandas',
                         'h5py',
                         'jsonschema',
-                        'google-api-python-client',
-                        'google-auth-httplib2',
-                        'google-auth-oauthlib',
                         'opencv-python', #opencv is required but naming differences between conda and pip seem to cause issues. Maybe should be commented out?
-                        'PyQt5',
                         'scikit-image',
                         'mpl_qt_viz>=1.0.5'],  # Plotting package available on PyPi and the backmanlab anaconda cloud account. Written for this project by Nick Anthony
       package_dir={'': 'src'},
       package_data={'pwspy': ['utility/reflection/refractiveIndexFiles/*',
                               'utility/thinFilmInterferenceFiles/*',
-                              'apps/_resources/*',
                               'analysis/_resources/defaultAnalysisSettings/*',
-                              'apps/PWSAnalysisApp/_resources/*',
                               'dataTypes/jsonSchemas/*',
                               '_version']},
-      packages=find_packages('src'),
-	  entry_points={'gui_scripts': [
-          'PWSAnalysis = pwspy.apps.PWSAnalysisApp.__main__:main',
-          "ERCreator = pwspy.apps.ExtraReflectanceCreator.__main__:main"
-      ]}
+      packages=find_packages('src')
 	)
