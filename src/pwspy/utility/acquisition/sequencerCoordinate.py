4ef35c442c9ff1551e293fe780ede9d1d12998c7
Nick Anthony <nicholas.anthony@northwestern.edu>
2021-03-09 09:31:52 -0600

    remove unnecessary class from sequencerCoordinate

--- src/pwspy/utility/acquisition/sequencerCoordinate.py
+++ src/pwspy/utility/acquisition/sequencerCoordinate.py
@@ -1,43 +1,32 @@
 from __future__ import annotations
 import json
-import typing
+import typing as t_
 import os
 from pwspy.dataTypes import AcqDir
-
-
-class SequencerCoordinateStep:
-    """The contribution to a sequencer coordinate from a single step"""
-    def __init__(self, id: int, iteration: int = None):
-        self.stepId = id  # All steps should have a unique id number
-        self.iteration = iteration  # Most steps will keep this as None, iterable steps will have an iteration.
-
-    def __eq__(self, other: SequencerCoordinateStep):
-        return self.stepId == other.stepId and self.iteration == other.iteration
-
-    def __repr__(self):
-        s = f"Step(ID:{self.stepId}"
-        if self.iteration is not None:
-            s += f", i:{self.iteration}"
-        s += ")"
-        return s
 
 
 class SequencerCoordinate:
     """
     A coordinate that fully defines a position within a `tree` of steps.
+
+    Args:
+        coordSteps: A sequence of tuples of the form (stepId, stepIteration) where `stepId` is the id number of the step being referred to.
+            If the step is an iterable step (multiple position, timeseries, etc.) then `stepIteration` should indicate the iteration number,
+            otherwise it should be `None`.
+        uuid: A universally unique ID string associated with the run of the sequencer that this coordinate is associated with.
     """
-    def __init__(self, coordSteps: typing.List[SequencerCoordinateStep], uuid: str):
-        self.fullPath = tuple(coordSteps)
+    def __init__(self, coordSteps: t_.Sequence[t_.Tuple[int, int]], uuid: str):
+        self._fullPath = tuple(coordSteps)
         self.uuid = uuid  # Matches the uuid of the sequence file that ran this acquisition.
 
     def __repr__(self):
-        return f"SeqCoord:{self.fullPath}"
+        return f"SeqCoord:{self._fullPath}"
 
     @staticmethod
     def fromDict(d: dict) -> SequencerCoordinate:
         c = []
-        for id, iteration in zip(d['treeIdPath'], d["stepIterations"]):
-            c.append(SequencerCoordinateStep(id, iteration))
+        for ID, iteration in zip(d['treeIdPath'], d["stepIterations"]):
+            c.append((ID, iteration))
         if 'uuid' in d:
             uuid = d['uuid']
         else:
@@ -52,37 +41,41 @@ class SequencerCoordinate:
     def isSubPathOf(self, other: SequencerCoordinate):
         """Check if `self` is a parent path of the `item` coordinate """
         assert isinstance(other, SequencerCoordinate)
-        if len(self.fullPath) >= len(other.fullPath):
+        if len(self._fullPath) >= len(other._fullPath):
             return False
-        return self.fullPath == other.fullPath[:len(self.fullPath)]
+        return self._fullPath == other._fullPath[:len(self._fullPath)]
 
     @property
-    def iterations(self) -> typing.Sequence[int]:
-        return tuple(i.iteration for i in self.fullPath)
+    def iterations(self) -> t_.Sequence[int]:
+        return tuple(iteration for ID, iteration in self._fullPath)
 
     @property
-    def ids(self) -> typing.Sequence[int]:
-        return tuple(i.stepId for i in self.fullPath)
+    def ids(self) -> t_.Sequence[int]:
+        return tuple(ID for ID, iteration in self._fullPath)
 
     def __eq__(self, other: SequencerCoordinate):
         """Check if these coordinates are identical"""
         assert isinstance(other, SequencerCoordinate)
-        return self.fullPath == other.fullPath
+        return self._fullPath == other._fullPath
 
 
 class IterationRangeCoordStep:
     """Represents a coordinate for a single step that accepts multiple iterations"""
-    def __init__(self, id: int, iterations: typing.Sequence[int] = None):
+    def __init__(self, id: int, iterations: t_.Sequence[int] = None):
         self.stepId = id
         self.iterations = iterations  #Only iterable step types will have this, most types will keep this as None
 
-    def __contains__(self, item: SequencerCoordinateStep):
-        if self.stepId == item.stepId:
+    def __contains__(self, item: t_.Tuple[int, int]):
+        """
+        Args:
+            item: A tuple of form (stepId, iteration). See the documentation for SequencerCoordinate
+        """
+        if self.stepId == item[0]:
             if self.iterations is None:  # This step doesn't have any iterations so there is no need to check anything.
                 return True
             elif len(self.iterations) == 0:  # If the accepted iterations are empty then we accept any iteration
                 return True
-            elif item.iteration in self.iterations:
+            elif item[1] in self.iterations:
                 return True
         return False
 
@@ -91,7 +84,7 @@ class SequencerCoordinateRange:
     """
     A coordinate that can have multiple iterations selected at once.
     """
-    def __init__(self, coordSteps: typing.Sequence[IterationRangeCoordStep]):
+    def __init__(self, coordSteps: t_.Sequence[IterationRangeCoordStep]):
         self.fullPath = tuple(coordSteps)
 
     def __contains__(self, item: SequencerCoordinate):
@@ -99,7 +92,7 @@ class SequencerCoordinateRange:
         if not isinstance(item, SequencerCoordinate):
             return False
         for i, coordRange in enumerate(self.fullPath):
-            if not (item.fullPath[i] in coordRange):
+            if not (item._fullPath[i] in coordRange):
                 return False
         return True
 
@@ -109,7 +102,7 @@ class SeqAcqDir(AcqDir):
     A subclasss of AcqDir that has will also search for a sequencerCoordinate file
     and load it as an attribute.
     """
-    def __init__(self, directory: typing.Union[str, AcqDir]):
+    def __init__(self, directory: t_.Union[str, AcqDir]):
         if isinstance(directory, AcqDir):
             directory = directory.filePath
         super().__init__(directory)
