6711daff6f78af25fe9225115304300acbd00684
Nick Anthony <nicholas.anthony@northwestern.edu>
2021-08-18 14:59:03 -0500

    update example.

--- src/pwspy/utility/micromanager/examples/positionTransformation.py
+++ src/pwspy/utility/micromanager/examples/positionTransformation.py
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with PWSpy.  If not, see <https://www.gnu.org/licenses/>.
+from pwspy.utility.micromanager import PropertyMap
 
 from pwspy.utility.micromanager.positions import PositionList
 
@@ -22,12 +23,22 @@ This method relies on measuring a set (at least 3) of reference positions before
 affine transform. This affine transform can then be applied to your original cell positions in order to generate a new set of positions for the same cells.
 In the case of a standard cell culture dish it is best to use the corners of the glass coverslip as your reference locations.
 """
-preTreatRefPositions = PositionList.load(r'experimentPath\preCorners.pos')  # Load the position list of the coverslip corners taken at the beginning of the experiment.
-postTreatRefPositions = PositionList.load(r'experimentPath\postCorners.pos') # Load the position list of the coverslip corners after placing the dish back on the microscope after treatment.
-transformMatrix = preTreatRefPositions.getAffineTransform(postTreatRefPositions)  # Generate an affine transform describing the difference between the two position lists.
-preTreatCellPositions = PositionList.load(r'experimentPath\position_list1.pos')  # Load the positions of the cells we are measuring before the dish was removed.
-postTreatCellPositions = preTreatCellPositions.applyAffineTransform(transformMatrix)  # Transform the cell positions to the new expected locations.
-postTreatCellPositions.save(r'experimentPath\transformedPositions.pos')  # Save the new positions to a file that can be loaded by Micro-Manager.
 
-preTreatRefPositions.plot()
-postTreatRefPositions.plot()
+if __name__ == '__main__':
+
+    import pathlib as pl
+    import matplotlib.pyplot as plt
+    plt.ion()
+    NCPath = pl.Path(r'Z:\Nick\NU-NC_EtOH fixation comparison\NC\Buccal cell')
+    NUPath = pl.Path(r'Z:\Nick\NU-NC_EtOH fixation comparison\NU\Buccal')
+
+    preTreatRefPositions = PositionList.fromNanoMatFile(NCPath / 'corners_list2.mat', 'TIXYDrive')  # Load the position list of the coverslip corners taken at the beginning of the experiment.
+    postTreatRefPositions = PositionList.fromPropertyMap(PropertyMap.loadFromFile(NUPath / 'corners.pos')) # Load the position list of the coverslip corners after placing the dish back on the microscope after treatment.
+    transformMatrix = preTreatRefPositions.getAffineTransform(postTreatRefPositions)  # Generate an affine transform describing the difference between the two position lists.
+    preTreatCellPositions = PositionList.fromNanoMatFile(NCPath / 'cell_list.mat', 'TIXYDrive')  # Load the positions of the cells we are measuring before the dish was removed.
+    postTreatCellPositions = preTreatCellPositions.applyAffineTransform(transformMatrix)  # Transform the cell positions to the new expected locations.
+    postTreatCellPositions.toPropertyMap().saveToFile(NUPath / 'transformedPositions.pos')  # Save the new positions to a file that can be loaded by Micro-Manager.
+
+    preTreatRefPositions.plot()
+    postTreatRefPositions.plot()
+    a = 1
